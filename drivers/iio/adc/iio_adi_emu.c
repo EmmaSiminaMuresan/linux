805836735d712c62dbf6f052bcc341ddff4fc2f5
Pop Ioan Daniel <Pop.Ioan-daniel@analog.com>
2023-08-07 12:45:55 +0300

    driver: Add support for ADI ADC Emu
    This is the initial commit for the ADI ADC Emulator driver.
    It contanuns the IIO structure and it is registered as a SPI driver.

---
+++ drivers/iio/adc/iio_adi_emu.c
@@ -0,0 +1,42 @@
+// SPDX-License-Identifier: GPL-2.0-only
+/*
+ * ADI ADC emulator driver
+ *
+ * Copyright 2023 Analog Devices Inc.
+ */
+
+#include <linux/module.h>
+#include <linux/spi/spi.h>
+
+#include <linux/iio/iio.h>
+
+static const struct iio_info adi_emu_info = {
+
+
+};
+static  int adi_emu_probe(struct spi_device *spi)
+{
+         struct iio_dev *indio_dev;
+
+	 indio_dev = devm_iio_device_alloc(&spi->dev, 0);
+	 if (!indio_dev)
+	 	return -ENOMEM; //mesaj de eroare Eroare de memorie
+
+	indio_dev->name = "iio_adi-emu";
+	indio_dev->info = &adi_emu_info;
+
+	return devm_iio_device_register(&spi->dev,indio_dev);	 
+}
+
+
+static struct spi_driver adi_emu_driver = {
+        .driver = {
+		.name = "iio-adi-emu", // similar cu nume fisier
+	},
+	.probe = adi_emu_probe,
+};
+module_spi_driver(adi_emu_driver);
+
+MODULE_AUTHOR ("Pop Ioan Daniel <Pop.Ioan-daniel@analog.com>");
+MODULE_DESCRIPTION("Analog Devices ADC Emulator Driver");
+MODULE_LICENSE("GPL v2");
