f08560de6d467580eaace0cee021d0b5a5cb9318
Vlad <vladcristescu1@gmail.com>
2023-08-07 12:42:06 +0300

    /linux_emu : Driver for ADC
    Wrote ADI ADC emulator driver and created Kconfig for it

---
+++ drivers/iio/adc/adi_emu.c
@@ -0,0 +1,42 @@
+// SPDX-License-Identifier: GPL-2.0-only
+/*
+ * ADI ADC emulator driver
+ *
+ * Copyright 2023 Analog Devices Inc.
+ */
+
+#include <linux/module.h>
+#include <linux/spi/spi.h>
+
+#include <linux/iio/iio.h>
+
+static const struct iio_info adi_emu_info = {
+
+};
+
+static int adi_emu_probe(struct spi_device *spi){
+	struct iio_dev *indio_dev;
+
+	indio_dev = devm_iio_device_alloc(&spi->dev, NULL);
+	if(!indio_dev)
+		return -ENOMEM;
+
+	indio_dev -> name = "iio-adi-emu";
+	indio_dev -> info = &adi_emu_info;
+
+	return devm_iio_device_register(&spi->dev,indio_dev);
+}
+
+static struct spi_driver adi_emu_driver = {
+	.driver  = {
+		.name = "iio-adi-emu",
+	},
+	.probe = adi_emu_probe,
+};
+module_spi_driver(adi_emu_driver);
+          
+
+
+MODULE_AUTHOR("Vlad Cristescu <vladcristescu1@gmail.com>");
+MODULE_DESCRIPTION("Analog Devices ADC Emulator driver");
+MODULE_LICENSE("GPL 0v2");
