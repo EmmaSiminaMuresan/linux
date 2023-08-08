65c867a6bcf730a22ebe52fddec3a65cb3a2b3f0
Bogdan Stanea <Adrian.Stanea@analog.com>
2023-08-08 15:57:25 +0300

    drivers/iio/adc : add raw channles to driver and read functionality
    - define input voltage channels that emulate the output of the adc that
      is read by the processing system
    - implement adi_read_raw function to process input at each channel with
      constant values
    - verify using quemu the implemented functionality by reading voltage
      channels from /sys/bus/iio/devices/iio:device0 using cat command
    
    Signed-off-by: Bogdan Stanea <Adrian.Stanea@analog.com>

--- drivers/iio/adc/iio-adi-emu.c
+++ drivers/iio/adc/iio-adi-emu.c
@@ -9,20 +9,57 @@
 #include <linux/iio/iio.h>
 #include <linux/spi/spi.h>
 
-static const struct iio_info adi_emu_info = {
+static int adi_emu_read_raw(struct iio_dev *indio_dev,
+			    struct iio_chan_spec const *chan,
+			    int *val,
+			    int *val2,
+			    long mask)
+{
+	switch (mask)
+	{
+	case IIO_CHAN_INFO_RAW:
+		if(chan->channel) {
+			*val = 10;
+		} else {
+			*val = 42;
+		}
+		return IIO_VAL_INT;
+	default:
+		return -EINVAL;
+	}
+}
 
+static const struct iio_info adi_emu_info = {
+	.read_raw =  &adi_emu_read_raw,
+};
+
+static const struct iio_chan_spec adi_emu_channels[] = {
+	{
+		.type = IIO_VOLTAGE,
+		.channel = 0,
+		.indexed = 1,// /sys/bus/iio canalul apare ca si channel_numar
+		.info_mask_separate = BIT(IIO_CHAN_INFO_RAW),
+	},
+	{
+		.type = IIO_VOLTAGE,
+		.channel = 1,
+		.indexed = 1,// /sys/bus/iio canalul apare ca si channel_numar
+		.info_mask_separate = BIT(IIO_CHAN_INFO_RAW),
+	}
 };
 
 static int adi_emu_probe(struct spi_device *spi)
 {
 	struct iio_dev *indio_dev;
 
-	indio_dev = devm_iio_device_alloc(&spi->dev, NULL);
+	indio_dev = devm_iio_device_alloc(&spi->dev, 0);
 	if(!indio_dev)
 		return -ENOMEM;
 
 	indio_dev->name = "iio-adi-emu";
 	indio_dev->info = &adi_emu_info;
+	indio_dev->channels = adi_emu_channels;
+	indio_dev->num_channels = ARRAY_SIZE(adi_emu_channels);
 
 	return devm_iio_device_register(&spi->dev, indio_dev);
 }
