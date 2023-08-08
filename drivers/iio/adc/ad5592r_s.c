2ca7bf3bee8e1cd61a229feb84b61caadf95e3dc
Bocioanca Alexandru-Teodor <alebocioanca123@gmail.com>
2023-08-08 12:14:07 +0300

    iio: Add support for AD5592R ADC
    - Created the device tree
    - Added the source code of the driver
    - Added entry in Kconfig
    - Added entry in Makefile

---
+++ drivers/iio/adc/ad5592r_s.c
@@ -0,0 +1,38 @@
+// SPDX-License-Identifier: GPL-2.0-only
+/*
+ * AD5592R Digital <-> Analog converters driver
+ * Link: https://wiki.analog.com/resources/fpga/docs/axi_adc_ip
+ *
+ * Copyright 2023 Analog Devices Inc.
+ */
+
+#include <linux/module.h>
+#include <linux/spi/spi.h>
+#include <linux/iio/iio.h>
+
+static const struct iio_info ad5592r_s_info = {
+};
+
+static int ad5592r_s_probe(struct spi_device *spi)
+{
+	struct iio_dev *indio_dev;
+	indio_dev = devm_iio_device_alloc(&spi->dev, 0);
+	if (!indio_dev)
+		return -ENOMEM;
+	indio_dev->name = "ad5592r_s";
+	indio_dev->info = &ad5592r_s_info;
+
+	return devm_iio_device_register(&spi->dev, indio_dev);
+}
+
+static struct spi_driver ad5592r_s_driver = {
+	.driver = {
+		.name = "ad5592r_s",
+	},
+	.probe = ad5592r_s_probe,
+};
+module_spi_driver(ad5592r_s_driver);
+
+MODULE_AUTHOR("Alexandru-Teodor Bocioanca <alexbocioanca123@gmail.com>");
+MODULE_DESCRIPTION("Analog Devices ADC Emulator Driver");
+MODULE_LICENSE("GPL v2");
