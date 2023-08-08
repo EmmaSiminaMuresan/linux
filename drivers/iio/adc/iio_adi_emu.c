922afb21c643118d2c2762d1b391938ee2319b65
nicupop09 <p.nicu173@gmail.com>
2023-08-08 16:13:09 +0300

    read_emu

--- drivers/iio/adc/iio_adi_emu.c
+++ drivers/iio/adc/iio_adi_emu.c
@@ -11,9 +11,44 @@
 
 #include <linux/iio/iio.h>
 
-static const struct iio_info adi_emu_info = {
+static int adi_emu_read_raw(struct iio_dev *indio_dev,
+			    struct iio_chan_spec const *chan,
+			    int *val,
+			    int *val2,
+			    long mask)
+{
+	switch (mask)	{
+	case IIO_CHAN_INFO_RAW:
+		if (chan->channel)
+			*val = 10;
+		else
+			*val = 22;
+		return IIO_VAL_INT;	
+	default:
+		return -EINVAL;
+	}
+}
 
+static const struct iio_info adi_emu_info = {
+	.read_raw = &adi_emu_read_raw,
 };
+
+static const struct iio_chan_spec adi_emu_chanel[] = {
+	{
+		.type = IIO_VOLTAGE,
+		.channel = 0,
+		.indexed = 1,
+		.info_mask_separate = BIT(IIO_CHAN_INFO_RAW),
+	},
+	{
+		.type = IIO_VOLTAGE,
+		.channel = 1,
+		.indexed = 1,
+		.info_mask_separate = BIT(IIO_CHAN_INFO_RAW),
+	},
+		
+};
+
 
 static int adi_emu_probe(struct spi_device *spi)
 {
@@ -25,6 +60,8 @@ static int adi_emu_probe(struct spi_device *spi)
 
 	indio_dev->name = "iio_adi_emu";
 	indio_dev->info = &adi_emu_info;
+	indio_dev->channels = adi_emu_chanel;
+	indio_dev->num_channels = ARRAY_SIZE(adi_emu_chanel);
 
 	return devm_iio_device_register(&spi->dev,indio_dev);
 }
