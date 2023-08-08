bb55ae7a5480cc38afe2245135cc84501f3e0f5b
ValentinDolcescu7 <valentindolcescu.vd@gmail.com>
2023-08-08 15:46:21 +0300

    iio/adc: Defined the channels specification structure
    The "iio-adi-emu" device has two 16-bit channels, each representing a single acquisition line. These channels are defined in the "adi_emu_channels" struct.
    The following details describe these channels:
    Type: The channel's measurement type is voltage. This is indicated by setting the type to "IIO_VOLTAGE."
    Info Mask Separate: Attributes specific to each channel are marked using this mask. Both channels have the "IIO_CHAN_INFO_RAW" attribute, where the value is shifted by 1 bit using the enum attribute.
    Info Mask Shared by All: Some attributes are shared by all channels, regardless of type or direction. The "IIO_CHAN_INFO_ENABLE" attribute is shared by both channels.
    Output/Input: These channels are used for input, as they represent data from the ADC. The "output" field is set to 0.
    Indexed: Channel attribute names include an index. This is true, so the "indexed" field is set to 1 for both channels.
    Channel Index: Each channel is assigned a unique index. The first channel has an index of 0, and the second channel has an index of 1.

--- drivers/iio/adc/iio_adi_emu.c
+++ drivers/iio/adc/iio_adi_emu.c
@@ -10,8 +10,47 @@
 
 #include <linux/iio/iio.h>
 
+static int adi_emu_read_raw(struct iio_dev *indio_dev,
+			    struct iio_chan_spec const *chan,
+			    int *val,
+			    int *val2,
+			    long mask)
+{
+	switch (mask)
+	{
+	case IIO_CHAN_INFO_RAW:
+	        if(chan->channel)
+		        *val = 10;
+		else    
+		        *val = 22;
+		return 	IIO_VAL_INT;	
+	default:
+		return -EINVAL;
+	}
+}
+
 
 static const struct iio_info  adi_emu_info = {
+     
+         .read_raw = &adi_emu_read_raw,
+};
+
+
+static const struct iio_chan_spec adi_emu_channels[] = {
+
+     {	 
+       .type = IIO_VOLTAGE,
+       .channel = 0,
+       .indexed =1,
+       .info_mask_separate = BIT(IIO_CHAN_INFO_RAW),
+     },
+
+    {	 
+       .type = IIO_VOLTAGE,
+       .channel = 1,
+       .indexed =1,
+       .info_mask_separate = BIT(IIO_CHAN_INFO_RAW),
+     },
 
 };
 
@@ -22,9 +61,13 @@ static int adi_emu_probe(struct spi_device *spi)
 	indio_dev = devm_iio_device_alloc(&spi->dev, 0);
 	if(!indio_dev)
 	           return  -ENOMEM;
+
        
         indio_dev->name = "iio-adi-emu";
 	indio_dev->info =  &adi_emu_info;
+	indio_dev->channels = adi_emu_channels;
+	indio_dev->num_channels =ARRAY_SIZE(adi_emu_channels);
+
         
 	return devm_iio_device_register(&spi->dev,indio_dev);
 }
