5922b1e78da5efa785b65ea5e2835a47cd334333
nicupop09 <p.nicu173@gmail.com>
2023-08-08 17:15:34 +0300

    Read_File_AD5592R

--- drivers/iio/adc/ad5592r_s.c
+++ drivers/iio/adc/ad5592r_s.c
@@ -1,6 +1,6 @@
 // SPDX-License-Identifier: GPL-2.0-only
 /*
- * ADI AD5592R ADC driver
+ * ADI ADC driver
  *
  * Copyright 2023 Analog Devices Inc.
  */
@@ -9,33 +9,64 @@
 #include <linux/spi/spi.h>
 
 #include <linux/iio/iio.h>
-
+static int ad5592r_read_raw(struct iio_dev *indio_dev,
+                            struct iio_chan_spec const *chan,
+                            int *val,
+                            int *val2,
+                            long mask)
+{
+        switch (mask)
+        {
+        case IIO_CHAN_INFO_RAW:
+                if(chan->channel)
+                        *val = 10;
+                else
+                        *val = 22;
+                return IIO_VAL_INT;
+        default:
+                return -EINVAL;
+        }
+}
 static const struct iio_info ad5592r_s_info = {
-
+        .read_raw = &ad5592r_read_raw,
 };
 
-static int ad5592r_s_probe(struct spi_device *spi)
+static const struct iio_chan_spec ad5592r_channel[] = {
+        {
+                .type = IIO_VOLTAGE,
+                .channel = 0,
+                .indexed = 1,
+                .info_mask_separate = BIT(IIO_CHAN_INFO_RAW),
+        },
+        {
+                .type = IIO_VOLTAGE,
+                .channel = 1,
+                .indexed = 1,
+                .info_mask_separate = BIT(IIO_CHAN_INFO_RAW),
+        },
+};
+
+static int ad5592r_probe(struct spi_device *spi)
 {
-	struct iio_dev *indio_dev;
-
-	indio_dev = devm_iio_device_alloc(&spi->dev, 0);
-	if (!indio_dev)
-		return -ENOMEM;
-	
-	indio_dev->name = "ad5592r_s";
-	indio_dev->info = &ad5592r_s_info;
-
-	return devm_iio_device_register(&spi->dev,indio_dev);
+        struct iio_dev *indio_dev;
+        indio_dev = devm_iio_device_alloc(&spi->dev, 0);
+        if (!indio_dev)
+                return -ENOMEM;
+        indio_dev->name = "ad5592r_s";
+        indio_dev->info = &ad5592r_s_info;
+        indio_dev->channels = ad5592r_channel;
+        indio_dev->num_channels = ARRAY_SIZE(ad5592r_channel);
+        return devm_iio_device_register(&spi->dev, indio_dev);
 }
 
 static struct spi_driver ad5592r_s_driver = {
-	.driver = {
-		.name = "ad5592r_s",
-	},
-	.probe = ad5592r_s_probe,
+        .driver = {
+                .name = "ad5592r_s",
+        },
+        .probe = ad5592r_probe,
 };
-module_spi_driver(ad5592r_s_driver);
 
-MODULE_AUTHOR("Ciprian Hegbeli <ciprian.hegbeli@analog.com");
-MODULE_DESCRIPTION("Analog Devices AD5592R ADC Driver");
+module_spi_driver(ad5592r_s_driver);
+MODULE_AUTHOR("Pop Nicolae-Adrian <p.nicu173@gmail.com>");
+MODULE_DESCRIPTION("Analog Devices ADC Emulator Driver");
 MODULE_LICENSE("GPL v2");
