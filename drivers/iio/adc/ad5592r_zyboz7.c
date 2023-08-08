c2ac485c5e74d95cc64c852b584bd575bab4615f
Voic Andrei <voicandrei22@gmail.com>
2023-08-08 16:36:40 +0300

    Add Read Functionality to AD5592R Zybo-Z7 Driver (2 channels)
    This commit introduces the read functionality for the AD5592R Zybo-Z7 driver. The implementation includes:
    
    - A `read_raw` function that handles reading raw voltage values for the two channels.
    - Channel specifications for voltage type, defining two channels with separate information masks for raw data.
    - Necessary structures and probe function to register the IIO device.
    
    Note: The current implementation returns hardcoded values for the channels and does not communicate with the actual hardware. Further integration with the specific hardware communication protocol is needed to fully operationalize the driver.
    
    Signed-off-by: Voic Andrei <voicandrei22@gmail.com>

--- drivers/iio/adc/ad5592r_zyboz7.c
+++ drivers/iio/adc/ad5592r_zyboz7.c
@@ -10,23 +10,59 @@
 
 #include <linux/iio/iio.h>
 
+static int adi5592r_zybo_read_raw(struct iio_dev *indio_dev,
+                                  struct iio_chan_spec const *chan,
+                                  int *val,
+                                  int *val2,
+                                  long mask)
+{
+    switch (mask) {
+    case IIO_CHAN_INFO_RAW:
+       		if (chan->channel)
+        		*val = 10;
+		else
+        		*val = 22; 
+        	return IIO_VAL_INT;
+    default:
+        	return -EINVAL;
+    }
+}
+
 static const struct iio_info adi5592r_zybo_info =  {
-   
+       .read_raw = &adi5592r_zybo_read_raw,
+};
+
+static const struct iio_chan_spec adi5592r_zybo_channel[] = {
+	{
+		.type = IIO_VOLTAGE,
+		.channel = 0,
+		.indexed = 1,
+		.info_mask_separate = BIT(IIO_CHAN_INFO_RAW),
+	},
+	{
+		.type = IIO_VOLTAGE,
+		.channel = 1,
+		.indexed = 1,
+		.info_mask_separate = BIT(IIO_CHAN_INFO_RAW),
+	}
 };
 
 static int adi5592r_zyboz7_probe(struct spi_device *spi)
 {
-    struct iio_dev *indio_dev;
+	struct iio_dev *indio_dev;
 
-    indio_dev = devm_iio_device_alloc(&spi->dev, 0);
+	indio_dev = devm_iio_device_alloc(&spi->dev, 0);
 	if (!indio_dev)
 		return -ENOMEM;
 
 	indio_dev->name = "iio-ad5592r-zyboz7";
-	indio_dev->info = &adi5592r_zybo_info; 
-	
+	indio_dev->info = &adi5592r_zybo_info;
+	indio_dev->channels = adi5592r_zybo_channel; // Use the new channel array
+	indio_dev->num_channels = ARRAY_SIZE(adi5592r_zybo_channel);
+
 	return devm_iio_device_register(&spi->dev, indio_dev);
 }
+
 
 static struct spi_driver adi5592r_zybo_driver = { 
 	.driver = {
