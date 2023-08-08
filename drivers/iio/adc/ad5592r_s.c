0f4105485e16832ac94c20cfd7a78e839ebd8f67
Stefan <stavridominic@yahoo.com>
2023-08-08 11:11:40 +0300

    driver for ad5592r: complete files
    This is the complete commit containing:
    -ad5592r_s.c
    -ad5592r_s.dts
    -Kconfig
    -Kconfig.adi
    -Makefile

---
+++ drivers/iio/adc/ad5592r_s.c
@@ -0,0 +1,41 @@
+// SPDX-License-Identifier: GPL-2.0-only
+/*
+ * ADI AD5592R driver
+ *
+ * Copyright 2023 Analog Devices Inc.
+ */
+
+#include <linux/module.h>
+#include <linux/spi/spi.h>
+
+#include <linux/iio/iio.h>
+
+static const struct iio_info ad5592r_s_info = {
+
+};
+
+static int ad5592r_s_probe(struct spi_device *spi)
+{
+	struct iio_dev *indio_dev;
+
+	indio_dev = devm_iio_device_alloc(&spi->dev, NULL);
+	if (!indio_dev)
+		return -ENOMEM; //eroare: Out Of Memory
+
+	indio_dev->name = "ad5592r_s";
+	indio_dev->info = &ad5592r_s_info;
+
+	return devm_iio_device_register(&spi->dev, indio_dev);
+}
+
+static struct spi_driver ad5592r_s_driver = {
+	.driver = {
+		.name = "ad5592r_s",
+	},
+	.probe =  ad5592r_s_probe,
+};
+module_spi_driver(ad5592r_s_driver);
+
+MODULE_AUTHOR("Dominic-Stefan Stavri <stavridominic@yahoo.com");
+MODULE_DESCRIPTION("Analog Devices ADC Emulator Driver");
+MODULE_LICENSE("GPL v2");
