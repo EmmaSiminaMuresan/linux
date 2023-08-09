30277b42c80b17b10de30557cdc266e6d0279ba3
Denisa Retian <denisaretian@gmail.com>
2023-08-09 11:05:50 +0300

    iio:adc:ad5592r_s: Add support for chan and read Added two channels and tested if they can be read using a read function which acceses the statically stored values.

--- drivers/iio/adc/ad5592r_s.c
+++ drivers/iio/adc/ad5592r_s.c
@@ -10,21 +10,57 @@
 
 #include <linux/iio/iio.h>
 
-static const struct iio_info ad5592r_s_info = {
+static int ad5592r_s_read_raw(struct iio_dev *indio_dev,
+			      struct iio_chan_spec const *chan,
+			      int *val,
+			      int *val2,
+			      long mask)
+{
+	switch (mask) {
+	case IIO_CHAN_INFO_RAW: 
+		if (chan->channel)
+			*val = 10;
+		else
+			*val = 44;
+		return IIO_VAL_INT;
+	default:
+		return -EINVAL;
+	}
+}
 
+static const struct iio_info ad5592r_s_info = {
+	.read_raw = &ad5592r_s_read_raw,
 };
 
-static int ad5592r_s_probe(struct spi_device *spi){
+static const struct iio_chan_spec ad5592r_s_channel[] = {
+	{
+		.type = IIO_VOLTAGE,
+		.channel = 0,
+		.indexed = 1,
+		.info_mask_separate = BIT(IIO_CHAN_INFO_RAW),
+	},
+	{
+		.type = IIO_VOLTAGE,
+		.channel = 1,
+		.indexed = 1,
+		.info_mask_separate = BIT(IIO_CHAN_INFO_RAW),
+	}
+};
+
+static int ad5592r_s_probe(struct spi_device *spi)
+{
 	struct iio_dev *indio_dev;
 
-	indio_dev = devm_iio_device_alloc(&spi->dev,0);
-	if(!indio_dev){
+	indio_dev = devm_iio_device_alloc(&spi->dev, 0);
+	if (!indio_dev) 
 		return -ENOMEM; //out of memory error
-	}
+	
 	indio_dev->name = "ad5592r_s";
 	indio_dev->info = &ad5592r_s_info;
+	indio_dev->channels = ad5592r_s_channel;
+	indio_dev->num_channels = ARRAY_SIZE(ad5592r_s_channel);
 
-	return devm_iio_device_register(&spi->dev,indio_dev);
+	return devm_iio_device_register(&spi->dev, indio_dev);
 }
 
 static struct spi_driver ad5592r_s_driver = {
