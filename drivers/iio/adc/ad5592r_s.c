33039e6497ff5fa099f90883845fd519e4d59d18
Tudor Radoni <tudor.radoni@gmail.com>
2023-08-09 12:22:15 +0300

    iio: adc: ad5592r_s: Add priv struct and write
    Added the write raw function to the private structure and an
    enable attribute. These attributes can be read and written.

--- drivers/iio/adc/ad5592r_s.c
+++ drivers/iio/adc/ad5592r_s.c
@@ -10,39 +10,69 @@
 
 #include <linux/iio/iio.h>
 
+// This is the state of the device
+struct ad5592r_s_state
+{
+	bool en;
+	u16 tmp_chan0;
+	u16 tmp_chan1;
+};
+
 // Read channel and, depending on the channel, return a value
 static int ad5592r_s_read_raw(struct iio_dev *indio_dev,
-			    struct iio_chan_spec const *chan,
-			    int *val,
-			    int *val2,
-			    long mask) {
+			      struct iio_chan_spec const *chan,
+			      int *val,
+			      int *val2,
+			      long mask)
+{
+	struct ad5592r_s_state *st = iio_priv(indio_dev);
 	switch (mask)
 	{
 	case IIO_CHAN_INFO_RAW:
-		if (chan->channel == 0) {
-			*val = 10;
-		} else if (chan->channel == 1) {
-			*val = 20;
-		} else if (chan->channel == 2) {
-			*val = 30;
-		} else if (chan->channel == 3) {
-			*val = 40;
-		} else if (chan->channel == 4) {
-			*val = 50;
-		} else if (chan->channel == 5) {
-			*val = 60;
-		}
+		if (chan->channel)
+			*val = st->tmp_chan1;
+		else
+			*val = st->tmp_chan0;
 		return IIO_VAL_INT;
-		break;
+
+	case IIO_CHAN_INFO_ENABLE:
+		*val = st->en;
+		return IIO_VAL_INT;
 
 	default:
 		return -EINVAL;
-		break;
+	}
+}
+
+// Write
+static int ad5592r_s_write_raw(struct iio_dev *indio_dev,
+			       struct iio_chan_spec const *chan,
+			       int val,
+			       int val2,
+			       long mask)
+{
+	struct ad5592r_s_state *st = iio_priv(indio_dev);
+	switch (mask)
+	{
+	case IIO_CHAN_INFO_RAW:
+		if (chan->channel)
+			st->tmp_chan1 = val;
+		else
+			st->tmp_chan0 = val;
+		return 0;
+
+	case IIO_CHAN_INFO_ENABLE:
+		st->en = val;
+		return 0;
+
+	default:
+		return -EINVAL;
 	}
 }
 
 static const struct iio_info ad5592r_s_info = {
-    .read_raw = &ad5592r_s_read_raw,
+	.read_raw = &ad5592r_s_read_raw,
+	.write_raw = &ad5592r_s_write_raw,
 };
 
 static const struct iio_chan_spec ad5592r_s_channel[] = {
@@ -51,49 +81,34 @@ static const struct iio_chan_spec ad5592r_s_channel[] = {
 		.channel = 0,
 		.indexed = 1,
 		.info_mask_separate = BIT(IIO_CHAN_INFO_RAW),
+		.info_mask_shared_by_all = BIT(IIO_CHAN_INFO_ENABLE),
 	},
 	{
 		.type = IIO_VOLTAGE,
 		.channel = 1,
 		.indexed = 1,
 		.info_mask_separate = BIT(IIO_CHAN_INFO_RAW),
-	},
-	{
-		.type = IIO_VOLTAGE,
-		.channel = 2,
-		.indexed = 1,
-		.info_mask_separate = BIT(IIO_CHAN_INFO_RAW),
-	},
-	{
-		.type = IIO_VOLTAGE,
-		.channel = 3,
-		.indexed = 1,
-		.info_mask_separate = BIT(IIO_CHAN_INFO_RAW),
-	},
-	{
-		.type = IIO_VOLTAGE,
-		.channel = 4,
-		.indexed = 1,
-		.info_mask_separate = BIT(IIO_CHAN_INFO_RAW),
-	},
-	{
-		.type = IIO_VOLTAGE,
-		.channel = 5,
-		.indexed = 1,
-		.info_mask_separate = BIT(IIO_CHAN_INFO_RAW),
+		.info_mask_shared_by_all = BIT(IIO_CHAN_INFO_ENABLE),
 	},
 };
 
-static int ad5592r_s_probe(struct spi_device *spi) {
+static int ad5592r_s_probe(struct spi_device *spi)
+{
 	struct iio_dev *indio_dev;
+	struct ad5592r_s_state *st;
 
-	indio_dev = devm_iio_device_alloc(&spi->dev, 0);
+	indio_dev = devm_iio_device_alloc(&spi->dev, sizeof(*st));
 
 	if (!indio_dev)
 		return -ENOMEM;
 
+	st = iio_priv(indio_dev);
+	st->en = 0;
+	st->tmp_chan0 = 0;
+	st->tmp_chan1 = 0;
+
 	indio_dev->name = "ad5592r_s";
-	indio_dev->info =  &ad5592r_s_info;
+	indio_dev->info = &ad5592r_s_info;
 	indio_dev->channels = ad5592r_s_channel;
 	indio_dev->num_channels = ARRAY_SIZE(ad5592r_s_channel);
 
