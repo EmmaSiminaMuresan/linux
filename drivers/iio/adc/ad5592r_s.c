6d23bae36e8334820063da6392f1b38480125228
Dragos Popa <dragos.popa777@gmail.com>
2023-08-09 09:46:28 +0300

    iio/adc/ad5592r_s.c : Added support for chan and read

--- drivers/iio/adc/ad5592r_s.c
+++ drivers/iio/adc/ad5592r_s.c
@@ -10,8 +10,74 @@
 
 #include <linux/iio/iio.h>
 
-static const struct iio_info ad5592r_s_info = {
+static int ad5592r_s_read_raw(struct iio_dev *indio_dev,
+			     struct iio_chan_spec const *chan,
+			     int *val,
+			     int *val2,
+			     long mask)
+{
+	switch (mask) {
+	case IIO_CHAN_INFO_RAW:
+		if (chan->channel == 5)
+			*val = 15;
+		else if (chan->channel == 4)
+			*val = 14;
+		else if (chan->channel == 3)
+			*val = 13;
+		else if (chan->channel == 2)
+			*val = 12;
+		else if (chan->channel == 1)
+			*val = 11;
+		else
+			*val = 10;
+		return IIO_VAL_INT;
+	
+	default:
+		return -EINVAL;
+	}
+}
 
+static const struct iio_info ad5592r_s_info = {
+	.read_raw = ad5592r_s_read_raw,
+};
+
+static const struct iio_chan_spec ad5592r_s_channel[] = {
+	{
+		.type = IIO_VOLTAGE,
+		.channel = 0,
+		.indexed = 1,
+		.info_mask_separate = BIT(IIO_CHAN_INFO_RAW),
+	},
+	{
+		.type = IIO_VOLTAGE,
+		.channel = 1,
+		.indexed = 1,
+		.info_mask_separate = BIT(IIO_CHAN_INFO_RAW),
+	},
+	{
+		.type = IIO_VOLTAGE,
+		.channel = 2,
+		.indexed = 1,
+		.info_mask_separate = BIT(IIO_CHAN_INFO_RAW),
+	},
+	{
+		.type = IIO_VOLTAGE,
+		.channel = 3,
+		.indexed = 1,
+		.info_mask_separate = BIT(IIO_CHAN_INFO_RAW),
+	},
+	{
+		.type = IIO_VOLTAGE,
+		.channel = 4,
+		.indexed = 1,
+		.info_mask_separate = BIT(IIO_CHAN_INFO_RAW),
+	},
+	{
+		.type = IIO_VOLTAGE,
+		.channel = 5,
+		.indexed = 1,
+		.info_mask_separate = BIT(IIO_CHAN_INFO_RAW),
+	},
 };
 
 static int ad5592r_s_probe(struct spi_device *spi)
@@ -24,6 +90,8 @@ static int ad5592r_s_probe(struct spi_device *spi)
 
 	indio_dev->name = "ad5592r-s";
 	indio_dev->info = &ad5592r_s_info;
+	indio_dev->channels = ad5592r_s_channel;
+	indio_dev->num_channels = ARRAY_SIZE(ad5592r_s_channel);
 
 	return devm_iio_device_register(&spi->dev, indio_dev);
 }
