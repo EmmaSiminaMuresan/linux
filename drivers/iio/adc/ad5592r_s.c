482b65549ced4eeb05dee543649ecd6722bf331f
Bontya Thomas <thomasbontya@gmail.com>
2023-08-09 10:43:33 +0300

    ad5592r_s
    Am adaugat chan si canalele si le-am facut functia read

--- drivers/iio/adc/ad5592r_s.c
+++ drivers/iio/adc/ad5592r_s.c
@@ -11,9 +11,51 @@
 
 #include <linux/iio/iio.h>
 
+static int adi_emu_read_raw(struct iio_dev *indio_dev,
+	 	 	    struct iio_chan_spec const *chan,
+			    int *val,
+			    int *val2,
+			    long mask)
+{
 
+switch (mask)
+{
+case IIO_CHAN_INFO_RAW:
+	switch(chan->channel)
+		{
+		case 0: *val = 1; break;
+	  	case 1: *val = 12; break;
+		case 2: *val = 13; break;
+		case 3: *val = 14; break;
+		case 4: *val = 15; break;
+		case 5: *val = 16; break;
+		}
+
+	return IIO_VAL_INT;
+
+default:
+	return -EINVAL;
+}
+
+}
 
 static const struct iio_info ad5592r_info = {
+	.read_raw = &adi_emu_read_raw,
+};
+
+static const struct iio_chan_spec adi_emu_channel[] = {
+	{
+		.type = IIO_VOLTAGE,
+		.channel = 0,
+		.indexed = 1, //seteaza canalul, e ca un true
+		.info_mask_separate = BIT(IIO_CHAN_INFO_RAW),
+	},
+	{
+		.type = IIO_VOLTAGE,
+		.channel = 1,
+		.indexed = 1, //seteaza canalul, e ca un true
+		.info_mask_separate = BIT(IIO_CHAN_INFO_RAW),
+	},
 
 };
 
@@ -27,6 +69,8 @@ static int ad5592r_probe(struct spi_device *spi)
 
 	indio_dev -> name = "iio-ad5592r";
 	indio_dev -> info = &ad5592r_info;
+	indio_dev -> channels = adi_emu_channel;
+	indio_dev -> num_channels = ARRAY_SIZE(adi_emu_channel);
 
 	return devm_iio_device_register(&spi->dev, indio_dev);
 
