45476e64426ea9ddf68ef86cb626eee81ec4a343
Voic Andrei <voicandrei22@gmail.com>
2023-08-09 13:52:29 +0300

    iio: adc: ad5592r-zyboz7: Introduce private state struct and write_raw function
    This commit incorporates several enhancements to the AD5592R Zybo-Z7 driver:
    
    1. A private state structure (`struct adi5592r_state`) is introduced to hold channel data and an enable attribute. This structure allows for better code organization and maintainability.
    
    2. The write_raw function is added, enabling writing to the IIO channels and the enable attribute. This new functionality aligns the driver with recent requirements.
    
    3. The read_raw function is updated to use the private state structure and to include the ability to read the enable attribute.
    
    4. The IIO channel specifications are updated to include the enable attribute.
    
    These changes collectively improve the flexibility and robustness of the driver, making it more adaptable to future updates.

--- drivers/iio/adc/ad5592r_zyboz7.c
+++ drivers/iio/adc/ad5592r_zyboz7.c
@@ -7,8 +7,13 @@
 
 #include <linux/module.h>
 #include <linux/spi/spi.h>
-
 #include <linux/iio/iio.h>
+
+struct adi5592r_state {
+	bool en;
+	u16 tmp_chan0;
+	u16 tmp_chan1;
+};
 
 static int adi5592r_zybo_read_raw(struct iio_dev *indio_dev,
                                   struct iio_chan_spec const *chan,
@@ -16,20 +21,49 @@ static int adi5592r_zybo_read_raw(struct iio_dev *indio_dev,
                                   int *val2,
                                   long mask)
 {
+	struct adi5592r_state *st = iio_priv(indio_dev);
+
     switch (mask) {
     case IIO_CHAN_INFO_RAW:
-       		if (chan->channel)
-        		*val = 10;
+		if (chan->channel)
+			*val = st->tmp_chan1;
 		else
-        		*val = 22; 
-        	return IIO_VAL_INT;
+			*val = st->tmp_chan0;
+        return IIO_VAL_INT;
+	case IIO_CHAN_INFO_ENABLE:
+		*val = st->en;
+		return IIO_VAL_INT;
     default:
-        	return -EINVAL;
+        return -EINVAL;
     }
 }
 
+static int adi5592r_zybo_write_raw(struct iio_dev *indio_dev,
+                                   struct iio_chan_spec const *chan,
+                                   int val,
+                                   int val2,
+                                   long mask)
+{
+	struct adi5592r_state *st = iio_priv(indio_dev);
+
+	switch (mask) {
+	case IIO_CHAN_INFO_RAW:
+		if (chan->channel)
+			st->tmp_chan1 = val;
+		else
+			st->tmp_chan0 = val;
+		return 0;
+	case IIO_CHAN_INFO_ENABLE:
+		st->en = val;
+		return 0;
+	default:
+		return -EINVAL;
+	}
+}
+
 static const struct iio_info adi5592r_zybo_info =  {
-       .read_raw = &adi5592r_zybo_read_raw,
+	.read_raw = &adi5592r_zybo_read_raw,
+	.write_raw = &adi5592r_zybo_write_raw,
 };
 
 static const struct iio_chan_spec adi5592r_zybo_channel[] = {
@@ -38,33 +72,40 @@ static const struct iio_chan_spec adi5592r_zybo_channel[] = {
 		.channel = 0,
 		.indexed = 1,
 		.info_mask_separate = BIT(IIO_CHAN_INFO_RAW),
+		.info_mask_shared_by_all = BIT(IIO_CHAN_INFO_ENABLE),
 	},
 	{
 		.type = IIO_VOLTAGE,
 		.channel = 1,
 		.indexed = 1,
 		.info_mask_separate = BIT(IIO_CHAN_INFO_RAW),
+		.info_mask_shared_by_all = BIT(IIO_CHAN_INFO_ENABLE),
 	}
 };
 
 static int adi5592r_zyboz7_probe(struct spi_device *spi)
 {
 	struct iio_dev *indio_dev;
+	struct adi5592r_state *st;
 
-	indio_dev = devm_iio_device_alloc(&spi->dev, 0);
+	indio_dev = devm_iio_device_alloc(&spi->dev, sizeof(*st));
 	if (!indio_dev)
 		return -ENOMEM;
 
+	st = iio_priv(indio_dev);
+	st->en = 0;
+	st->tmp_chan0 = 0;
+	st->tmp_chan1 = 0;
+
 	indio_dev->name = "iio-ad5592r-zyboz7";
 	indio_dev->info = &adi5592r_zybo_info;
-	indio_dev->channels = adi5592r_zybo_channel; // Use the new channel array
+	indio_dev->channels = adi5592r_zybo_channel;
 	indio_dev->num_channels = ARRAY_SIZE(adi5592r_zybo_channel);
 
 	return devm_iio_device_register(&spi->dev, indio_dev);
 }
 
-
-static struct spi_driver adi5592r_zybo_driver = { 
+static struct spi_driver adi5592r_zybo_driver = {
 	.driver = {
 		.name = "iio-ad5592r-zyboz7",
 	},
