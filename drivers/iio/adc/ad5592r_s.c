614a07742ee6f386df85760be227a292c3d44fe3
Analog <adinailisei6>
2023-08-09 11:56:39 +0300

    iio: Add write function for AD5592R ADC

--- drivers/iio/adc/ad5592r_s.c
+++ drivers/iio/adc/ad5592r_s.c
@@ -9,12 +9,17 @@
 
 #include <linux/iio/iio.h>
 
+struct ad5592r_state {
+        bool en;
+};
+
 static int ad5592r_read_raw(struct iio_dev *indio_dev,
                             struct iio_chan_spec const *chan,
                             int *val,
                             int *val2,
                             long mask)
 {
+	struct ad5592r_state *st = iio_priv(indio_dev);
 	switch (mask)
 	{
 	case IIO_CHAN_INFO_RAW:
@@ -22,15 +27,41 @@ static int ad5592r_read_raw(struct iio_dev *indio_dev,
 			*val = 10;
 		else
 			*val = 22;
+
 		return IIO_VAL_INT;
+
+	case IIO_CHAN_INFO_ENABLE:
+                *val = st->en;
+
+                return IIO_VAL_INT;
 	
 	default:
 		return -EINVAL;
 	}
 }
 
+static int ad5592r_write_raw(struct iio_dev *indio_dev,
+                            struct iio_chan_spec const *chan,
+                            int val,
+                            int val2,
+                            long mask)
+{
+        struct ad5592r_state *st = iio_priv(indio_dev);
+        switch (mask)
+        {
+        case IIO_CHAN_INFO_RAW:
+                st->en = val;
+	case IIO_CHAN_INFO_ENABLE:
+		st->en = val;
+		return 0;
+        default:
+                return -EINVAL;
+        }
+}
+
 static const struct iio_info ad5592r_s_info = {
 	.read_raw = &ad5592r_read_raw,
+	.write_raw = &ad5592r_write_raw,
 };
 
 static const struct iio_chan_spec ad5592r_channel[] = {
@@ -39,6 +70,7 @@ static const struct iio_chan_spec ad5592r_channel[] = {
 		.channel = 0,
 		.indexed = 1,
 		.info_mask_separate = BIT(IIO_CHAN_INFO_RAW),
+		.info_mask_shared_by_all = BIT(IIO_CHAN_INFO_ENABLE),
 	},
 
 	{
@@ -46,6 +78,7 @@ static const struct iio_chan_spec ad5592r_channel[] = {
 		.channel = 1,
 		.indexed = 1,
 		.info_mask_separate = BIT(IIO_CHAN_INFO_RAW),
+		.info_mask_shared_by_all = BIT(IIO_CHAN_INFO_ENABLE),
 	},
 
 };
@@ -53,10 +86,14 @@ static const struct iio_chan_spec ad5592r_channel[] = {
 static int ad5592r_probe(struct spi_device *spi)
 {
         struct iio_dev *indio_dev;
+	struct ad5592r_state *st;
 
         indio_dev = devm_iio_device_alloc(&spi->dev, 0);
         if (!indio_dev)
                 return -ENOMEM;
+		
+	st = iio_priv(indio_dev);
+        st->en = 0;
 
         indio_dev->name = "ad5592r_s";
         indio_dev->info = &ad5592r_s_info;
