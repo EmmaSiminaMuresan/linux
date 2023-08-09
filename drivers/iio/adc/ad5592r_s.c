24e16f5d4d9391216ae591586c21b89af752c851
Vlad <vladcristescu1@gmail.com>
2023-08-09 15:34:59 +0300

    iio : adc: ad5592r_s: Create 6 channels
    Added the missing channels for all the ADC inputs and resolved a few bugs

--- drivers/iio/adc/ad5592r_s.c
+++ drivers/iio/adc/ad5592r_s.c
@@ -10,21 +10,93 @@
 
 #include <linux/iio/iio.h>
 
+struct ad5592r_s_state {
+	bool en;
+	u16 tmp_chan0;
+	u16 tmp_chan1;
+	u16 tmp_chan2;
+	u16 tmp_chan3;
+	u16 tmp_chan4;
+	u16 tmp_chan5;
+};
+
 static int ad5592r_s_read_raw(struct iio_dev *indio_dev,
 			    struct iio_chan_spec const *chan,
 			    int *val,
 			    int *val2,
 			    long mask)
 {
-	switch (mask)
-	{
+	struct ad5592r_s_state *st = iio_priv(indio_dev);
+
+	switch (mask){
 	case IIO_CHAN_INFO_RAW:
-		if(chan->channel)
-			*val = 5;
-		else	
-			*val = 12;
+		switch (chan->channel){
+		case 5:
+			*val = st->tmp_chan5;
+			break;
+		case 4:
+			*val = st->tmp_chan4;
+			break;
+		case 3:
+			*val = st->tmp_chan3;
+			break;
+		case 2:
+			*val = st->tmp_chan2;
+			break;
+		case 1:
+			*val = st->tmp_chan1;
+			break;				
+		case 0:
+			*val = st->tmp_chan0;
+			break;
+		default:
+			return -EINVAL;
+		}
 		return IIO_VAL_INT;	
-	
+	case IIO_CHAN_INFO_ENABLE:
+			*val = st->en;
+		return IIO_VAL_INT;
+	default:
+		return -EINVAL;
+	}
+}
+
+static int ad5592r_s_write_raw(struct iio_dev *indio_dev,
+			     struct iio_chan_spec const *chan,
+			     int val,
+			     int val2,
+			     long mask)
+{
+	struct ad5592r_s_state *st = iio_priv(indio_dev);
+
+	switch (mask){
+	case IIO_CHAN_INFO_RAW:
+		switch (chan->channel){
+		case 5:
+			st->tmp_chan5 = val;
+			break;
+		case 4:
+			st->tmp_chan4 = val;
+			break;
+		case 3:
+			st->tmp_chan3 = val;
+			break;
+		case 2:
+			st->tmp_chan2 = val;
+			break;
+		case 1:
+			st->tmp_chan1 = val;
+			break;	
+		case 0:	
+			st->tmp_chan0 = val;
+			break;			
+		default:
+			return -EINVAL;
+		}
+		return 0;
+	case IIO_CHAN_INFO_ENABLE:
+		st->en = val ;
+		return 0;
 	default:
 		return -EINVAL;
 	}
@@ -32,6 +104,7 @@ static int ad5592r_s_read_raw(struct iio_dev *indio_dev,
 
 static const struct iio_info ad5592r_s_info = {
 	.read_raw = &ad5592r_s_read_raw,
+	.write_raw = &ad5592r_s_write_raw,
 };
 
 static const struct iio_chan_spec ad5592r_s_channel[] = {
@@ -40,21 +113,60 @@ static const struct iio_chan_spec ad5592r_s_channel[] = {
 		.channel = 0,
 		.indexed = 1,
 		.info_mask_separate = BIT(IIO_CHAN_INFO_RAW),
+		.info_mask_shared_by_all = BIT(IIO_CHAN_INFO_ENABLE),
 	},
 	{
 		.type = IIO_VOLTAGE,
 		.channel = 1,
 		.indexed = 1,
 		.info_mask_separate = BIT(IIO_CHAN_INFO_RAW),
-	}
+	},
+	{
+
+		.type = IIO_VOLTAGE,
+		.channel = 2,
+		.indexed = 1,
+		.info_mask_separate = BIT(IIO_CHAN_INFO_RAW),
+	},
+	{
+
+		.type = IIO_VOLTAGE,
+		.channel = 3,
+		.indexed = 1,
+		.info_mask_separate = BIT(IIO_CHAN_INFO_RAW),
+	},
+	{
+
+		.type = IIO_VOLTAGE,
+		.channel = 4,
+		.indexed = 1,
+		.info_mask_separate = BIT(IIO_CHAN_INFO_RAW),
+	},
+	{
+
+		.type = IIO_VOLTAGE,
+		.channel = 5,
+		.indexed = 1,
+		.info_mask_separate = BIT(IIO_CHAN_INFO_RAW),
+	},
 };
 
 static int ad5592r_s_probe(struct spi_device *spi){
 	struct iio_dev *indio_dev;
+	struct ad5592r_s_state *st;
 
-	indio_dev = devm_iio_device_alloc(&spi->dev, NULL);
+	indio_dev = devm_iio_device_alloc(&spi->dev, sizeof(*st));
 	if(!indio_dev)
 		return -ENOMEM;
+
+	st = iio_priv(indio_dev);
+	st->en = 0;
+	st->tmp_chan0 = 0;
+	st->tmp_chan1 = 0;
+	st->tmp_chan2 = 0;
+	st->tmp_chan3 = 0;
+	st->tmp_chan4 = 0;
+	st->tmp_chan5 = 0;	
 
 	indio_dev -> name = "ad5592r_s";
 	indio_dev -> info = &ad5592r_s_info;
@@ -71,8 +183,6 @@ static struct spi_driver ad5592r_s_driver = {
 	.probe = ad5592r_s_probe,
 };
 module_spi_driver(ad5592r_s_driver);
-          
-
 
 MODULE_AUTHOR("Vlad Cristescu <vladcristescu1@gmail.com>");
 MODULE_DESCRIPTION("Analog Devices AD5592r driver");
