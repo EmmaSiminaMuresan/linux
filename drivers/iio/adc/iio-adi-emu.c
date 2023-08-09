562341178b2db9a569e7d1be1695343c9f6c6d4b
Andrei-Fabian-Pop <Andreifabian.Pop@analog.com>
2023-08-09 10:43:54 +0300

    iio: adc: iio-adi-emu: Add priv struct and write
    Signed-off-by: Andrei-Fabian-Pop <Andreifabian.Pop@analog.com>

--- drivers/iio/adc/iio-adi-emu.c
+++ drivers/iio/adc/iio-adi-emu.c
@@ -1,20 +1,50 @@
 #include <linux/module.h>
 #include <linux/spi/spi.h>
 #include <linux/iio/iio.h>
+//#include <stdbool.h>
+//#include <asm-generic/errno.h>
+//#include "../../../include/linux/iio/types.h"
+//#include "../../../include/linux/iio/iio.h"
+//#include "../industrialio-core.c"
+
+struct adi_emu_state {
+	bool en;
+};
 
 int adi_emu_read_raw(struct iio_dev *indio_dev,
 		     struct iio_chan_spec const *chan,
 		     int *val,
 		     int *val2,
-		     long mask) 
+		     long mask)
 {
+	struct adi_emu_state *st = iio_priv(indio_dev);
 	switch (mask) {
 		case IIO_CHAN_INFO_RAW:
-			if (chan->channel)
+			if (chan->channel) {
 				*val = 10;
-			else
+			} else {
 				*val = 22;
+			}
 			return IIO_VAL_INT;
+		case IIO_CHAN_INFO_ENABLE:
+			*val = st->en;
+			return IIO_VAL_INT;
+		default:
+			return -EINVAL;
+	}
+}
+
+int adi_emu_write_raw(struct iio_dev *indio_dev,
+		      struct iio_chan_spec const *chan,
+		      int val,
+		      int val2,
+		      long mask)
+{
+	struct adi_emu_state *st = iio_priv(indio_dev);
+	switch (mask) {
+		case IIO_CHAN_INFO_ENABLE:
+			st->en = val;
+			return 0;
 		default:
 			return -EINVAL;
 	}
@@ -22,6 +52,7 @@ int adi_emu_read_raw(struct iio_dev *indio_dev,
 
 static const struct iio_info adi_emu_info = {
 	.read_raw = &adi_emu_read_raw,
+	.write_raw = &adi_emu_write_raw,
 };
 
 static const struct iio_chan_spec adi_emu_channel[] = {
@@ -29,23 +60,28 @@ static const struct iio_chan_spec adi_emu_channel[] = {
 		.type = IIO_VOLTAGE,
 		.channel = 0,
 		.indexed = 1,
-		.info_mask_separate = BIT(IIO_CHAN_INFO_RAW)
+		.info_mask_separate = BIT(IIO_CHAN_INFO_RAW),
+		.info_mask_shared_by_all = BIT(IIO_CHAN_INFO_ENABLE)
 	},
 	{
 		.type = IIO_VOLTAGE,
 		.channel = 1,
 		.indexed = 1,
-		.info_mask_separate = BIT(IIO_CHAN_INFO_RAW)
+		.info_mask_separate = BIT(IIO_CHAN_INFO_RAW),
+		.info_mask_shared_by_all = BIT(IIO_CHAN_INFO_ENABLE)
 	}
-
 };
 
-static int adi_emu_probe(struct spi_device *spi)
-{
+static int adi_emu_probe(struct spi_device *spi) {
 	struct iio_dev *indio_dev;
-	indio_dev = devm_iio_device_alloc(&spi->dev, 0);
+	struct adi_emu_state *pv;
+
+	indio_dev = devm_iio_device_alloc(&spi->dev, sizeof(*pv));
 	if (!indio_dev)
 		return -ENOMEM;
+
+	pv = iio_priv(indio_dev);
+	pv->en = 0;
 
 	indio_dev->name = "iio-adi-emu";
 	indio_dev->info = &adi_emu_info;
