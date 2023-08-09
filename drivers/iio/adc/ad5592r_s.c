2692ff30c7b908ae30b25d2c2e943f6846c90b49
nicupop09 <p.nicu173@gmail.com>
2023-08-09 13:20:58 +0300

    iio: adc: ad5592_r: Add 6 chanels

--- drivers/iio/adc/ad5592r_s.c
+++ drivers/iio/adc/ad5592r_s.c
@@ -6,13 +6,18 @@
  */
 
 #include <linux/module.h>
-
 #include <linux/spi/spi.h>
 
 #include <linux/iio/iio.h>
 
-struct ad5592r_state {
-        bool en;
+struct ad5592r_state{
+	bool en;
+	u16 tmp_chan0;
+	u16 tmp_chan1;
+	u16 tmp_chan2;
+	u16 tmp_chan3;
+	u16 tmp_chan4;
+	u16 tmp_chan5;
 };
 
 static int ad5592r_read_raw(struct iio_dev *indio_dev,
@@ -22,79 +27,101 @@ static int ad5592r_read_raw(struct iio_dev *indio_dev,
                             long mask)
 {
 	struct ad5592r_state *st = iio_priv(indio_dev);
-	switch (mask)
-	{
-	case IIO_CHAN_INFO_RAW:
-		if(chan->channel)
-			*val = 10;
-		else
-			*val = 22;
-		return IIO_VAL_INT;
-	case IIO_CHAN_INFO_ENABLE:
-                *val = st->en;
-                return IIO_VAL_INT;
-	
-	default:
-		return -EINVAL;
-	}
-}
 
-static int ad5592r_write_raw(struct iio_dev *indio_dev,
-                            struct iio_chan_spec const *chan,
-                            int val,
-                            int val2,
-                            long mask)
-{
-        struct ad5592r_state *st = iio_priv(indio_dev);
         switch (mask)
         {
         case IIO_CHAN_INFO_RAW:
-                st->en = val;
+                switch(chan->channel)
+                       {
+			case 0: *val = st->tmp_chan0; break;
+                        case 1: *val = st->tmp_chan1; break; 
+		        case 2: *val = st->tmp_chan2; break;
+		        case 3: *val = st->tmp_chan3; break;
+		        case 4: *val = st->tmp_chan4; break;
+		        case 5: *val = st->tmp_chan5; break;
+			}
+
+                return IIO_VAL_INT;
 	case IIO_CHAN_INFO_ENABLE:
-		st->en = val;
-		return 0;
+		*val =st->en;
+		return IIO_VAL_INT;
         default:
                 return -EINVAL;
         }
 }
 
+static int ad5592r_write_raw(struct iio_dev *indio_dev,
+			     struct iio_chan_spec const *chan,
+			     int val,
+			     int val2,
+			     long mask)
+{
+	struct ad5592r_state *st = iio_priv(indio_dev);
+
+	switch(mask)
+	{
+	case IIO_CHAN_INFO_RAW:
+		 switch(chan->channel)
+                       {
+			case 0: st->tmp_chan0 = val; break;
+        		case 1: st->tmp_chan1 = val; break; 
+			case 2: st->tmp_chan2 = val; break;
+			case 3: st->tmp_chan3 = val; break;
+			case 4: st->tmp_chan4 = val; break;
+			case 5: st->tmp_chan5 = val; break;
+			}
+
+		return 0;
+	default:
+		return -EINVAL;
+	}
+}
+
 static const struct iio_info ad5592r_s_info = {
-	.read_raw = &ad5592r_read_raw,
+        .read_raw = &ad5592r_read_raw,
 	.write_raw = &ad5592r_write_raw,
 };
 
 static const struct iio_chan_spec ad5592r_channel[] = {
-	{
-		.type = IIO_VOLTAGE,
-		.channel = 0,
-		.indexed = 1,
-		.info_mask_separate = BIT(IIO_CHAN_INFO_RAW),
+        {
+                .type = IIO_VOLTAGE,
+                .channel = 0,
+                .indexed = 1,
+                .info_mask_separate = BIT(IIO_CHAN_INFO_RAW),
 		.info_mask_shared_by_all = BIT(IIO_CHAN_INFO_ENABLE),
-	},
-	{
-		.type = IIO_VOLTAGE,
-		.channel = 1,
-		.indexed = 1,
-		.info_mask_separate = BIT(IIO_CHAN_INFO_RAW),
+        },
+        {
+                .type = IIO_VOLTAGE,
+                .channel = 1,
+                .indexed = 1,
+                .info_mask_separate = BIT(IIO_CHAN_INFO_RAW),
 		.info_mask_shared_by_all = BIT(IIO_CHAN_INFO_ENABLE),
-	},
+        },
 };
 
 static int ad5592r_probe(struct spi_device *spi)
 {
         struct iio_dev *indio_dev;
 	struct ad5592r_state *st;
-        indio_dev = devm_iio_device_alloc(&spi->dev, 0);
+
+        indio_dev = devm_iio_device_alloc(&spi->dev, sizeof(*st));
         if (!indio_dev)
                 return -ENOMEM;
-
-	st = iio_priv(indio_dev);
-        st->en = 0;
 	
+	st = iio_priv(indio_dev);
+	st->en = 0;
+	st->tmp_chan0 = 0;
+	st->tmp_chan1 = 0;
+	st->tmp_chan2 = 0;
+	st->tmp_chan3 = 0;
+	st->tmp_chan4 = 0;
+	st->tmp_chan5 = 0;
+
         indio_dev->name = "ad5592r_s";
         indio_dev->info = &ad5592r_s_info;
-	indio_dev->channels = ad5592r_channel;
-	indio_dev->num_channels = ARRAY_SIZE(ad5592r_channel);
+        indio_dev->channels = ad5592r_channel;
+        indio_dev->num_channels = ARRAY_SIZE(ad5592r_channel);
+
         return devm_iio_device_register(&spi->dev, indio_dev);
 }
 
@@ -106,7 +133,6 @@ static struct spi_driver ad5592r_s_driver = {
 };
 
 module_spi_driver(ad5592r_s_driver);
-
 MODULE_AUTHOR("Pop Nicolae-Adrian <p.nicu173@gmail.com>");
 MODULE_DESCRIPTION("Analog Devices ADC Emulator Driver");
 MODULE_LICENSE("GPL v2");
