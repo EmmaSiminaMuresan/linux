a7d636805e9ce9033f60fe5d3edb53590886327e
nicupop09 <p.nicu173@gmail.com>
2023-08-09 11:08:57 +0300

    iio: adc: iio_adi_emu: Bug fixing for write
    Added functionality for write rau function. Now the attribute can be now read and write.

--- drivers/iio/adc/iio_adi_emu.c
+++ drivers/iio/adc/iio_adi_emu.c
@@ -1,6 +1,6 @@
 // SPDX-License-Identifier: GPL-2.0-only
 /*
- * ADI ADC driver
+ * ADAQ8092 driver
  *
  * Copyright 2023 Analog Devices Inc.
  */
@@ -10,70 +10,107 @@
 #include <linux/spi/spi.h>
 
 #include <linux/iio/iio.h>
+struct adi_emu_state {
+        bool en;
+        u16 tmp_chan0;
+        u16 tmp_chan1;
+};
 
 static int adi_emu_read_raw(struct iio_dev *indio_dev,
-			    struct iio_chan_spec const *chan,
-			    int *val,
-			    int *val2,
-			    long mask)
+                            struct iio_chan_spec const *chan,
+                            int *val,
+                            int *val2,
+                            long mask)
 {
-	switch (mask)	{
-	case IIO_CHAN_INFO_RAW:
-		if (chan->channel)
-			*val = 10;
-		else
-			*val = 22;
-		return IIO_VAL_INT;	
-	default:
-		return -EINVAL;
-	}
+        struct adi_emu_state *st = iio_priv(indio_dev);
+        switch (mask)
+        {
+        case IIO_CHAN_INFO_RAW:
+                if(chan->channel)
+                        *val = st->tmp_chan1;
+                else
+                        *val = st->tmp_chan0;
+                return IIO_VAL_INT;
+        case IIO_CHAN_INFO_ENABLE:
+                *val = st->en;
+                return IIO_VAL_INT;
+        default:
+                return -EINVAL;
+        }
 }
-
+static int adi_emu_write_raw(struct iio_dev *indio_dev,
+                            struct iio_chan_spec const *chan,
+                            int val,
+                            int val2,
+                            long mask)
+{
+        struct adi_emu_state *st = iio_priv(indio_dev);
+        switch (mask)
+        {
+        case IIO_CHAN_INFO_RAW:
+                if(chan->channel)
+                        st ->tmp_chan1 = val;
+                else
+                        st-> tmp_chan0= val;
+                return 0;
+        case IIO_CHAN_INFO_ENABLE:
+                st->en = val;
+                return 0;
+        default:
+                return -EINVAL;
+        }
+}
 static const struct iio_info adi_emu_info = {
-	.read_raw = &adi_emu_read_raw,
+        .read_raw = &adi_emu_read_raw,
+        .write_raw = &adi_emu_write_raw,
 };
 
-static const struct iio_chan_spec adi_emu_chanel[] = {
-	{
-		.type = IIO_VOLTAGE,
-		.channel = 0,
-		.indexed = 1,
-		.info_mask_separate = BIT(IIO_CHAN_INFO_RAW),
-	},
-	{
-		.type = IIO_VOLTAGE,
-		.channel = 1,
-		.indexed = 1,
-		.info_mask_separate = BIT(IIO_CHAN_INFO_RAW),
-	},
-		
+static const struct iio_chan_spec adi_emu_channel[] = {
+        {
+                .type = IIO_VOLTAGE,
+                .channel = 0,
+                .indexed = 1,
+                .info_mask_separate = BIT(IIO_CHAN_INFO_RAW),
+                .info_mask_shared_by_all = BIT(IIO_CHAN_INFO_ENABLE),
+        },
+        {
+                .type = IIO_VOLTAGE,
+                .channel = 1,
+                .indexed = 1,
+                .info_mask_separate = BIT(IIO_CHAN_INFO_RAW),
+                .info_mask_shared_by_all = BIT(IIO_CHAN_INFO_ENABLE),
+        },
 };
-
 
 static int adi_emu_probe(struct spi_device *spi)
 {
-	struct iio_dev *indio_dev;
+        struct iio_dev *indio_dev;
+        struct adi_emu_state *st;
+        indio_dev = devm_iio_device_alloc(&spi->dev, sizeof (*st));
+        if (!indio_dev)
+                return -ENOMEM;
 
-	indio_dev = devm_iio_device_alloc(&spi->dev, 0);
-	if (!indio_dev)
-		return -ENOMEM;
-
-	indio_dev->name = "iio_adi_emu";
-	indio_dev->info = &adi_emu_info;
-	indio_dev->channels = adi_emu_chanel;
-	indio_dev->num_channels = ARRAY_SIZE(adi_emu_chanel);
-
-	return devm_iio_device_register(&spi->dev,indio_dev);
+        st = iio_priv(indio_dev);
+        st->en = 0;
+        st->tmp_chan1 = 0;
+        st->tmp_chan0 = 0;
+	
+        indio_dev->name = "iio_adi_emu";
+        indio_dev->info = &adi_emu_info;
+        indio_dev->channels = adi_emu_channel;
+        indio_dev->num_channels = ARRAY_SIZE(adi_emu_channel);
+        return devm_iio_device_register(&spi->dev,indio_dev);
 }
 
 static struct spi_driver adi_emu_driver = {
-	.driver = {
-		.name = "iio_adi_emu",
-	},
-	.probe = adi_emu_probe,
+        .driver = {
+                .name = "iio_adi_emu",
+        },
+        .probe = adi_emu_probe,
 };
+
 module_spi_driver(adi_emu_driver);
 
-MODULE_AUTHOR("Pop Nicolae-Adrian <p.nicu173@gmail.com");
+MODULE_AUTHOR("Pop Nicolae-ADrian <p.nicu173@gmail.com>");
 MODULE_DESCRIPTION("Analog Devices ADC Emulator Driver");
 MODULE_LICENSE("GPL v2");
