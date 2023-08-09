ddf99326d52893a4fb6ffd1b7775ecf6a02f848b
Beletei Nora <beleteinora4@gmail.com>
2023-08-09 12:04:17 +0300

    iio : adc: ad5592r_s : Defined private struture and write_raw fcn
    Added write_raw function
    Created a private struct
    Declared the private structure in the probe function
    Added the private structure functionality in read_raw and write_raw
    functions

--- drivers/iio/adc/ad5592r_s.c
+++ drivers/iio/adc/ad5592r_s.c
@@ -10,6 +10,13 @@
  
  #include <linux/iio/iio.h>
 
+struct ad5592r_s_state {
+	bool en;
+	u16 tmp_chan0;
+	u16 tmp_chan1;
+
+};
+
 static int ad5592r_s_read_raw(struct iio_dev *indio_dev,		
 			    struct iio_chan_spec const *chan, 
                  	    int *val, 
@@ -17,13 +24,18 @@ static int ad5592r_s_read_raw(struct iio_dev *indio_dev,
         		    long mask)
 
 {
+	struct ad5592r_s_state *st = iio_priv(indio_dev);
 	switch (mask) { 
    
     	case IIO_CHAN_INFO_RAW: 
 		if (chan->channel) 
-			*val = 10; 
+			*val = st->tmp_chan1; 
 		else 
-			*val = 22; 
+			*val = st->tmp_chan0; 
+		return IIO_VAL_INT;
+
+	case IIO_CHAN_INFO_ENABLE:
+		*val = st->en;
 		return IIO_VAL_INT;
 	default:
 		return -EINVAL;
@@ -32,8 +44,34 @@ static int ad5592r_s_read_raw(struct iio_dev *indio_dev,
 
 }
 
+static int ad5592r_s_write_raw(struct iio_dev *indio_dev,		
+			     struct iio_chan_spec const *chan, 
+                 	     int val, 
+                	     int val2, 
+        		     long mask)
+{
+	struct ad5592r_s_state *st = iio_priv(indio_dev);
+	switch(mask){
+		case IIO_CHAN_INFO_RAW: 
+		if (chan->channel) 
+			st->tmp_chan1 = val; 
+		else 
+			st->tmp_chan0 = val;
+		return 0;
+		case IIO_CHAN_INFO_ENABLE :
+		st->en = val;
+		return 0;
+
+		default:
+			return -EINVAL;
+	}
+}
+
+		
+
 static struct iio_info ad5592r_s_info = {
 	.read_raw = &ad5592r_s_read_raw,
+	.write_raw = &ad5592r_s_write_raw,
 
 };
 
@@ -43,39 +81,47 @@ static const struct iio_chan_spec ad5592r_s_channel[] = {
 		.channel = 0,
 		.indexed = 1,
 		.info_mask_separate = BIT(IIO_CHAN_INFO_RAW),
+		.info_mask_shared_by_all = BIT(IIO_CHAN_INFO_ENABLE),
 	},	
 		{
 		.type = IIO_VOLTAGE,
 		.channel = 1,
 		.indexed = 1,
 		.info_mask_separate = BIT(IIO_CHAN_INFO_RAW),
+		.info_mask_shared_by_all = BIT(IIO_CHAN_INFO_ENABLE),
 	},	
 
 };
 
+static int ad5592r_s_probe(struct spi_device *spi)
+{
+	struct iio_dev *indio_dev;
+	struct ad5592r_s_state *st;
 
- static int ad5592r_s_probe(struct spi_device *spi)
- {
-     struct iio_dev *indio_dev;
-     indio_dev = devm_iio_device_alloc(&spi->dev, 0);
-     if (!indio_dev)
-         return -ENOMEM;
- 
-     indio_dev->name = "ad5592r_s";
-     indio_dev->info = &ad5592r_s_info;
-     indio_dev->channels = ad5592r_s_channel ;
-     indio_dev->num_channels = ARRAY_SIZE(ad5592r_s_channel);
- 
-     return devm_iio_device_register(&spi->dev, indio_dev);	
- }
- 
- static struct spi_driver ad5592r_s_driver = {
-     .driver = {
-         .name = "ad5592r_s",
-     },
-     .probe = ad5592r_s_probe,
- };
- module_spi_driver(ad5592r_s_driver);
+	indio_dev = devm_iio_device_alloc(&spi->dev, sizeof(*st));
+	if (!indio_dev)
+		return -ENOMEM;
+
+	st = iio_priv(indio_dev);
+	st->en = 0;
+	st->tmp_chan0 = 0;
+	st->tmp_chan1 = 0;
+
+	indio_dev->name = "ad5592r_s";
+	indio_dev->info	= &ad5592r_s_info;
+	indio_dev->channels = ad5592r_s_channel ;
+	indio_dev->num_channels = ARRAY_SIZE(ad5592r_s_channel);
+
+	return devm_iio_device_register(&spi->dev, indio_dev);	
+}
+
+static struct spi_driver ad5592r_s_driver = {
+	.driver = {
+		.name = "ad5592r_s",
+	},
+	.probe = ad5592r_s_probe,
+};
+module_spi_driver(ad5592r_s_driver);
  
  MODULE_AUTHOR("Beletei Nora <beleteinora4@gmail.com>");
  MODULE_DESCRIPTION("Analog Devices ad5592r Driver");
