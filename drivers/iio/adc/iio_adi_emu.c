e0ab0be44e0faa886d6c600f976534174d723774
Analog <copacelnarcismihail@gmail.com>
2023-08-09 10:49:35 +0300

    Adaugare enable in iio_adi_emu

--- drivers/iio/adc/iio_adi_emu.c
+++ drivers/iio/adc/iio_adi_emu.c
@@ -10,6 +10,10 @@
 
 #include <linux/spi/spi.h>
 
+struct adi_emu_state  {
+	bool en;
+};
+
 
 static	int adi_emu_read_raw(	struct iio_dev *indio_dev,
 				struct iio_chan_spec const *chan,
@@ -17,6 +21,9 @@ static	int adi_emu_read_raw(	struct iio_dev *indio_dev,
 				int *val2,
 				long mask)
 {
+
+	struct adi_emu_state *st=iio_priv(indio_dev);
+
 	switch (mask)
 	{
 	case IIO_CHAN_INFO_RAW:
@@ -25,6 +32,29 @@ static	int adi_emu_read_raw(	struct iio_dev *indio_dev,
 		else
 			*val = 22;
 		return IIO_VAL_INT;
+	case IIO_CHAN_INFO_ENABLE:
+		*val = st->en;
+		return IIO_VAL_INT;
+	
+	default:
+		return -EINVAL;
+	}
+}
+
+static	int adi_emu_write_raw(	struct iio_dev *indio_dev,
+				struct iio_chan_spec const *chan,
+				int val,
+				int val2,
+				long mask)
+{
+
+	struct adi_emu_state *st=iio_priv(indio_dev);
+
+	switch (mask)
+	{
+	case IIO_CHAN_INFO_ENABLE:
+		st->en = val;
+		return 0;
 	
 	default:
 		return -EINVAL;
@@ -33,7 +63,9 @@ static	int adi_emu_read_raw(	struct iio_dev *indio_dev,
 
 static const struct iio_info adi_emu_info ={
 	.read_raw = &adi_emu_read_raw,
+	.write_raw = &adi_emu_write_raw,
 };
+
 
 
 static const struct iio_chan_spec adi_emu_channel[]={
@@ -42,23 +74,30 @@ static const struct iio_chan_spec adi_emu_channel[]={
 		.channel = 0,
 		.indexed = 1,
 		.info_mask_separate = BIT(IIO_CHAN_INFO_RAW),
+		.info_mask_shared_by_all = BIT(IIO_CHAN_INFO_ENABLE),
 	},
 	{
 		.type = IIO_VOLTAGE,
 		.channel = 1,
 		.indexed = 1,
 		.info_mask_separate = BIT(IIO_CHAN_INFO_RAW),
+		.info_mask_shared_by_all = BIT(IIO_CHAN_INFO_ENABLE),	
 	},
 
-};
+};	
 
 static int adi_emu_probe(struct spi_device *spi)
 {
 	struct iio_dev *indio_dev;
-	indio_dev=devm_iio_device_alloc(&spi->dev,0);
+	struct adi_emu_state *st;
+	
+
+	indio_dev=devm_iio_device_alloc(&spi->dev,sizeof(*st));
 	if(!indio_dev)
 		return -ENOMEM;
 	
+	st = iio_priv(indio_dev);
+	st->en = 0;
 	indio_dev->name = "iio-adi-emu";
 	indio_dev->info = &adi_emu_info;
 	indio_dev->channels = adi_emu_channel;
