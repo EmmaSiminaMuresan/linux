68a0fb975858a00e108ff8b19209ff542e374c0e
Narcis Copăcel <copacelnarcismihail@gmail.com>
2023-08-11 15:17:26 +0300

    Am creat functiile specifice pentru citirea folosind SPI

--- drivers/iio/adc/iio_adi_emu.c
+++ drivers/iio/adc/iio_adi_emu.c
@@ -1,120 +1,199 @@
-// SPDX-License-Identifier: GPL-2.0-or-later
+// SPDX-License-Identifier: GPL-2.0
 /*
- * Freescale i.MX7D ADC driver
+ * ADI ADC emulator driver
  *
- * Copyright (C) 2023 Freescale Semiconductor, Inc.
+ * Copyright 2016 Analog Devices Inc.
  */
-
+#include <linux/bitfield.h>
 #include <linux/module.h>
+#include <linux/spi/spi.h>
+#include <asm/unaligned.h>
 #include <linux/iio/iio.h>
 
-#include <linux/spi/spi.h>
+#define ADI_EMU_RD_MASK		BIT(7)
+#define ADI_EMU_ADDR_MASK	GENMASK(14, 8)
+#define ADI_EMU_VAL_MASK 	GENMASK(7, 0)
 
-struct adi_emu_state  {
+struct adi_emu_state {
+	struct spi_device *spi;
 	bool en;
+	u16 tmp_chan0;
+	u16 tmp_chan1;
 };
 
-
-static	int adi_emu_read_raw(	struct iio_dev *indio_dev,
-				struct iio_chan_spec const *chan,
-				int *val,
-				int *val2,
-				long mask)
+static int adi_emu_spi_read(struct adi_emu_state *st, u8 reg, u8 *val)
 {
+	u8 tx = 0;
+	u8 rx = 0;
+	int ret;
 
-	struct adi_emu_state *st=iio_priv(indio_dev);
+	 struct spi_transfer xfer[] = {
+		{
+			.tx_buf = NULL,
+			.rx_buf = NULL,
+			.len = 1
+		},
+		{
+			.tx_buf = NULL,
+			.rx_buf = NULL,
+			.len = 1
+		},
+	 };
+
+	tx = reg;
+	tx |= ADI_EMU_RD_MASK;
+
+	xfer[0].tx_buf = &tx;
+	xfer[1].rx_buf = &rx;
+	
+	ret = spi_sync_transfer(st->spi, xfer, 2);
+	if(ret)
+		return ret;
+
+	*val = rx;
+	return 0;
+
+}
+
+static int adi_emu_spi_write(struct adi_emu_state *st, u8 reg, u8 val)
+{
+	u16 msg = 0;
+	u16 tx = 0;
+	struct spi_transfer xfer = {
+		.tx_buf = NULL,
+		.len = 2,
+	};
+
+	msg |= FIELD_PREP(ADI_EMU_ADDR_MASK, reg);
+	msg |= FIELD_PREP(ADI_EMU_VAL_MASK, val);
+
+	put_unaligned_be16(msg, &tx);
+
+	xfer.tx_buf = &tx;
+
+	return spi_sync_transfer(st->spi, &xfer, 1);
+}
+
+static int adi_emu_read_raw(struct iio_dev *indio_dev,
+	 	 	    struct iio_chan_spec const *chan,
+			    int *val,
+			    int *val2,
+			    long mask)
+{
+	struct adi_emu_state *st = iio_priv(indio_dev);
 
 	switch (mask)
 	{
 	case IIO_CHAN_INFO_RAW:
 		if(chan->channel)
-			*val = 10;
+			*val = st->tmp_chan1;
 		else
-			*val = 22;
+			*val = st->tmp_chan0;
 		return IIO_VAL_INT;
+
 	case IIO_CHAN_INFO_ENABLE:
 		*val = st->en;
 		return IIO_VAL_INT;
-	
+
 	default:
 		return -EINVAL;
 	}
+
 }
 
-static	int adi_emu_write_raw(	struct iio_dev *indio_dev,
-				struct iio_chan_spec const *chan,
-				int val,
-				int val2,
-				long mask)
+static int adi_emu_write_raw(struct iio_dev *indio_dev,
+	 	 	    struct iio_chan_spec const *chan,
+			    int val,
+			    int val2,
+			    long mask)
 {
-
-	struct adi_emu_state *st=iio_priv(indio_dev);
+	struct adi_emu_state *st = iio_priv(indio_dev);
 
 	switch (mask)
-	{
+{
+	case IIO_CHAN_INFO_RAW:
+		if(chan->channel)
+			st->tmp_chan1 = val;
+		else
+			st->tmp_chan0 = val;
+		return 0;
 	case IIO_CHAN_INFO_ENABLE:
 		st->en = val;
 		return 0;
-	
 	default:
 		return -EINVAL;
-	}
+}
 }
 
-static const struct iio_info adi_emu_info ={
+static int adi_emu_reg_acces(struct iio_dev *indio_dev,
+			     unsigned reg, unsigned writeval,
+			     unsigned *readval)
+{
+	struct adi_emu_state *st = iio_priv(indio_dev);
+
+	if (readval)
+		return adi_emu_spi_read(st, reg, (u8 *)readval);
+
+	return adi_emu_spi_write(st, reg, writeval);
+}
+
+
+static const struct iio_info adi_emu_info = {
 	.read_raw = &adi_emu_read_raw,
 	.write_raw = &adi_emu_write_raw,
+	.debugfs_reg_access = &adi_emu_reg_acces,
 };
 
-
-
-static const struct iio_chan_spec adi_emu_channel[]={
+static const struct iio_chan_spec adi_emu_channel[] = {
 	{
 		.type = IIO_VOLTAGE,
 		.channel = 0,
-		.indexed = 1,
+		.indexed = 1, //seteaza canalul, e ca un true
 		.info_mask_separate = BIT(IIO_CHAN_INFO_RAW),
 		.info_mask_shared_by_all = BIT(IIO_CHAN_INFO_ENABLE),
 	},
 	{
 		.type = IIO_VOLTAGE,
 		.channel = 1,
-		.indexed = 1,
+		.indexed = 1, //seteaza canalul, e ca un true
 		.info_mask_separate = BIT(IIO_CHAN_INFO_RAW),
-		.info_mask_shared_by_all = BIT(IIO_CHAN_INFO_ENABLE),	
+		.info_mask_shared_by_all = BIT(IIO_CHAN_INFO_ENABLE),
 	},
 
-};	
+};
 
 static int adi_emu_probe(struct spi_device *spi)
 {
 	struct iio_dev *indio_dev;
 	struct adi_emu_state *st;
-	
 
-	indio_dev=devm_iio_device_alloc(&spi->dev,sizeof(*st));
-	if(!indio_dev)
+	indio_dev = devm_iio_device_alloc(&spi->dev, sizeof(*st));
+	if (!indio_dev)
 		return -ENOMEM;
 	
-	st = iio_priv(indio_dev);
+	st = iio_priv(indio_dev); 
+	st->spi = spi; 
 	st->en = 0;
-	indio_dev->name = "iio-adi-emu";
-	indio_dev->info = &adi_emu_info;
-	indio_dev->channels = adi_emu_channel;
-	indio_dev->num_channels = ARRAY_SIZE(adi_emu_channel);
+	st->tmp_chan0 = 0;
+	st->tmp_chan1 = 0;
 
-	return devm_iio_device_register(&spi->dev,indio_dev);
+	indio_dev -> name = "iio-adi-emu";
+	indio_dev -> info = &adi_emu_info;
+	indio_dev -> channels = adi_emu_channel;
+	indio_dev -> num_channels = ARRAY_SIZE(adi_emu_channel);
+
+
+	return devm_iio_device_register(&spi->dev, indio_dev);
 }
 
-static struct spi_driver adi_emu_driver=
-{
+static struct spi_driver adi_emu_driver = {
 	.driver = {
-		.name="iio-adi-emu",
+		.name = "iio-adi-emu",
 	},
 	.probe = adi_emu_probe,
 };
 module_spi_driver(adi_emu_driver);
 
-MODULE_AUTHOR("Copacel Narcis-MIhail<copacelnarcismihail@gmail.com>");
-MODULE_DESCRIPTION("Analog Devices ADC Emulator Driver");
-MODULE_LICENSE("GPL v2");
+ MODULE_AUTHOR("Copacel Narcis-Mihail <copacelnarcismihail@gmail.com>");
+ MODULE_DESCRIPTION("Analog Devices ADC Emulator Driver");
+ MODULE_LICENSE("GPL v2");
