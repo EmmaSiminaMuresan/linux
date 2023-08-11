c6c1e6b93839988dd95019e3f5a03fc47a3d1628
Dragos Popa <dragos.popa777@gmail.com>
2023-08-11 12:42:46 +0300

    iio/adc/iio_adc_emu.c : Added register read/write functions
    Added read and write functions to modify register values
    and a register access function

--- drivers/iio/adc/iio_adi_emu.c
+++ drivers/iio/adc/iio_adi_emu.c
@@ -5,16 +5,82 @@
  * Copyright 2023 Analog Devices Inc.
  */
 
+#include <asm/unaligned.h>
+#include <linux/bitfield.h>
 #include <linux/module.h>
 #include <linux/spi/spi.h>
 
 #include <linux/iio/iio.h>
 
+#define ADI_EMU_RD_MASK		BIT(7)
+#define ADI_EMU_ADDR_MASK	GENMASK(14, 8)
+#define ADI_EMU_VAL_MASK	GENMASK(7, 0)
+
 struct adi_emu_state {
+	struct spi_device *spi;
 	bool en;
 	u16 tmp_chan0;
 	u16 tmp_chan1;
 };
+
+static int adi_emu_spi_read(struct adi_emu_state *st,
+			    u8 reg,
+			    u8 *val)
+{
+	u8 tx = 0;
+	u8 rx = 0;
+	int ret;
+
+	struct spi_transfer xfer[] = {
+		{
+			.tx_buf = NULL,
+			.rx_buf = NULL,
+			.len = 1,
+			.cs_change = 1
+		},
+		{
+			.tx_buf = NULL,
+			.rx_buf = NULL,
+			.len = 1,
+			//.cs_change = 1
+		},
+	};
+
+
+	tx |= ADI_EMU_RD_MASK;
+	tx |= reg;
+
+	xfer[0].tx_buf = &tx;
+	xfer[1].rx_buf = &rx;
+
+	ret = spi_sync_transfer(st->spi, xfer, 2);
+	if (ret)
+		return ret;
+
+	*val = rx;
+	return 0;
+}
+
+static int adi_emu_spi_write(struct adi_emu_state *st,
+			     u8 reg,
+			     u8 val)
+{
+	u16 msg = 0;
+	u16 tx = 0;
+	struct spi_transfer xfer = {
+		.tx_buf = NULL,
+		.len = 2,
+	};
+
+	msg |= FIELD_PREP(ADI_EMU_ADDR_MASK, reg);
+	msg |= FIELD_PREP(ADI_EMU_VAL_MASK, val);
+
+	put_unaligned_be16(msg, &tx); 
+
+	xfer.tx_buf = &tx;
+
+	return spi_sync_transfer(st->spi, &xfer, 1);
+}
 
 static int adi_emu_read_raw(struct iio_dev *indio_dev,
 			    struct iio_chan_spec const *chan,
@@ -62,9 +128,23 @@ static int adi_emu_write_raw(struct iio_dev *indio_dev,
 	}				
 }
 
+static int adi_emu_reg_acces(struct iio_dev *indio_dev,
+			     unsigned reg,
+			     unsigned writeval,
+			     unsigned *readval)
+{
+	struct adi_emu_state *st = iio_priv(indio_dev);
+
+	if(readval)
+		return adi_emu_spi_read(st, reg, (u8 *)readval);
+
+	return adi_emu_spi_write(st, reg, writeval);
+}
+
 static const struct iio_info adi_emu_info = {
 	.read_raw = &adi_emu_read_raw,
 	.write_raw = &adi_emu_write_raw,
+	.debugfs_reg_access = &adi_emu_reg_acces,
 };
 
 static const struct iio_chan_spec adi_emu_channel[] = {
@@ -92,6 +172,7 @@ static int adi_emu_probe(struct spi_device *spi)
 	if(!indio_dev)
 		return -ENOMEM;
 	st = iio_priv(indio_dev);
+	st->spi = spi;
 	st->en = 0;
 	st->tmp_chan0 = 0;
 	st->tmp_chan1 = 0;
