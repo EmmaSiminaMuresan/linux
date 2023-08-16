2721f69be9f1143766261dc61e073f700bd7cecf
Tudor Radoni <tudor.radoni@gmail.com>
2023-08-16 12:33:29 -0400

    iio: adc: ad5592r_s: Add read and write over SPI
    Added functionality for reading from and writing to registers
    over SPI. This commit also contains register access through
    the kernel debug interface.

--- drivers/iio/adc/ad5592r_s.c
+++ drivers/iio/adc/ad5592r_s.c
@@ -5,18 +5,96 @@
  * Copyright 2023 Analog Devices Inc.
  */
 
+#include <asm/unaligned.h>
+#include <linux/bitfield.h>
 #include <linux/module.h>
 #include <linux/spi/spi.h>
 
 #include <linux/iio/iio.h>
 
+#define AD5592R_S_REG_READBACK	0x7
+#define  AD5592R_S_MASK_RDB_EN	BIT(6)
+#define  AD5592R_S_MASK_RDB_REG	GENMASK(5, 2)
+
+#define AD5592R_S_ADDR_MASK	GENMASK(14, 11)
+#define AD5592R_S_VAL_MASK	GENMASK(10, 0)
+
 struct ad5592r_s_state
 {
+	struct spi_device *spi;
 	bool en;
 	u16 tmp_chan[6];
 };
 
-// Read channel and, depending on the channel, return a value
+static int ad5592r_s_spi_nop(struct ad5592r_s_state *st, u16 *val)
+{
+	struct spi_transfer xfer = {
+		.tx_buf = 0,
+		.rx_buf = val,
+		.len = 2,
+	};
+
+	return spi_sync_transfer(st->spi, &xfer, 1);
+}
+
+static int ad5592r_s_spi_read_ctl(struct ad5592r_s_state *st, u8 reg, u16 *val)
+{
+	u16 tx = 0;
+	u16 msg = 0;
+	u16 rx = 0;
+	int ret = 0;
+
+	struct spi_transfer xfer = {
+		.tx_buf = &tx,
+		.len = 2,
+	};
+
+	msg  = FIELD_PREP(AD5592R_S_ADDR_MASK, AD5592R_S_REG_READBACK);
+	msg |= AD5592R_S_MASK_RDB_EN;
+	msg |= FIELD_PREP(AD5592R_S_MASK_RDB_REG, reg);
+
+	put_unaligned_be16(msg, &tx);
+
+	// make spi transfer
+	ret = spi_sync_transfer(st->spi, &xfer, 1);
+	if (ret) {
+		dev_err(&st->spi->dev, "Failed at SPI WR transfer\n");
+		return ret;
+	}
+
+	ret = ad5592r_s_spi_nop(st, &rx);
+	if (ret) {
+		dev_err(&st->spi->dev, "Failed at SPI WR NOP transfer\n");
+		return ret;
+	}
+	
+	*val = get_unaligned_be16(&rx);
+	return 0;
+}
+
+// Write to a register
+static int ad5592r_s_spi_write(struct ad5592r_s_state *st, u8 reg, u16 val)
+{
+	u16 msg = 0;
+	u16 tx = 0;
+
+	struct spi_transfer xfer = {
+		.tx_buf = &tx,
+		.len = 2, // 
+	};
+
+	// set register address and value
+	msg  = FIELD_PREP(AD5592R_S_ADDR_MASK, reg);
+	msg |= FIELD_PREP(AD5592R_S_VAL_MASK, val);
+	// dev_info(&st->spi->dev, "msg at write: 0x%x\n", msg);
+
+	// convert to big endian
+	put_unaligned_be16(msg, &tx);
+	// dev_info(&st->spi->dev, "tx at write: 0x%x\n", tx);
+
+	return spi_sync_transfer(st->spi, &xfer, 1);
+}
+
 static int ad5592r_s_read_raw(struct iio_dev *indio_dev,
 			      struct iio_chan_spec const *chan,
 			      int *val,
@@ -107,9 +185,23 @@ static int ad5592r_s_write_raw(struct iio_dev *indio_dev,
 	}
 }
 
+static int ad5592r_s_reg_access(struct iio_dev *indio_dev,
+			      	unsigned reg,
+			      	unsigned writeval,
+			      	unsigned *readval)
+{
+	struct ad5592r_s_state *st = iio_priv(indio_dev);
+
+	if (readval)
+		return ad5592r_s_spi_read_ctl(st, reg, (u16 *)readval);
+	
+	return ad5592r_s_spi_write(st, reg, writeval);
+}
+
 static const struct iio_info ad5592r_s_info = {
 	.read_raw = &ad5592r_s_read_raw,
 	.write_raw = &ad5592r_s_write_raw,
+	.debugfs_reg_access = &ad5592r_s_reg_access,
 };
 
 static const struct iio_chan_spec ad5592r_s_channel[] = {
@@ -175,10 +267,7 @@ static int ad5592r_s_probe(struct spi_device *spi)
 	st->tmp_chan[3] = 0;
 	st->tmp_chan[4] = 0;
 	st->tmp_chan[5] = 0;
-
-	// for (int i = 0; i < 6; ++i) {
-	// 	st->tmp_chan[i] = 0;
-	// }
+	st->spi = spi;
 
 	indio_dev->name = "ad5592r_s";
 	indio_dev->info = &ad5592r_s_info;
