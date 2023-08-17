ae56e81fa13e2031f4a754391917a43bd57e6551
Bocioanca Alexandru-Teodor <alebocioanca123@gmail.com>
2023-08-17 17:51:04 +0300

    iio: adc: ad5592r_s: Add ADC sample read to iio
    Implemented the ADC read function and added it to the IIO structure by
    reading the raw values from user-space.

--- drivers/iio/adc/ad5592r_s.c
+++ drivers/iio/adc/ad5592r_s.c
@@ -11,13 +11,27 @@
 #include <linux/module.h>
 #include <linux/spi/spi.h>
 #include <linux/iio/iio.h>
+#include <linux/delay.h>
 
-#define AD5592R_S_REG_READBACK 0x7
-#define AD5592R_S_MASK_RDB_EN  BIT(6)
-#define AD5592R_S_MASK_RDB_REG GENMASK(5, 2)
+#define AD5592R_S_REG_ADC_SEQ		0x2
+#define AD5592R_S_REG_ADC_CFG		0x4
+#define AD5592R_S_REG_READBACK 		0x7
+#define AD5592R_S_MASK_RDB_EN 		BIT(6)
+#define AD5592R_S_MASK_RDB_REG 		GENMASK(5, 2)
 
-#define AD5592R_S_ADDR_MASK    GENMASK(14, 11)
-#define AD5592R_S_VAL_MASK     GENMASK(10, 0)
+#define AD5592R_S_ADDR_MASK 		GENMASK(14, 11)
+#define AD5592R_S_VAL_MASK 		GENMASK(10, 0)
+
+#define AD5592R_S_ADC_CHAN(x)		BIT(x)
+#define AD5592R_S_ADC_DEF_PIN_MASK 	GENMASK(5, 0)
+#define AD5592R_S_ADC_ADDR_MASK 	GENMASK(14, 12)
+#define AD5592R_S_ADC_VAL_MASK 		GENMASK(11, 0)
+
+#define AD5592R_S_REG_REF_CFG		0xB
+#define	AD5592R_S_MASK_REF_EN		BIT(9)
+
+#define AD5592R_S_REG_SW_RST		0xF
+#define AD5592R_S_MASK_RST		0x5AC
 
 struct ad5592r_s_state {
 	struct spi_device *spi;
@@ -89,21 +103,81 @@ static int ad5592r_s_spi_read_ctl(struct ad5592r_s_state *st, u8 reg, u16 *val)
 	return 0;
 }
 
+static int ad5592r_s_spi_read_adc(struct ad5592r_s_state *st, u8 chan, int *val)
+{
+	u16 msg = 0;
+	u16 tx = 0;
+	u16 rx = 0;
+	int ret;
+	u16 tmp;
+	u16 addr;
+
+	struct spi_transfer xfer = {
+		.tx_buf = &tx,
+		.len = 2,
+	};
+
+	msg |= FIELD_PREP(AD5592R_S_ADDR_MASK, AD5592R_S_REG_ADC_SEQ);
+	msg |= AD5592R_S_ADC_CHAN(chan);
+
+	put_unaligned_be16(msg, &tx);
+
+	ret = spi_sync_transfer(st->spi, &xfer, 1);
+	if(ret)
+	{
+		dev_err(&st->spi->dev, "Failed at SEQ REG WR");
+		return ret;
+	}
+
+	ret = ad5592r_s_nop(st, NULL);
+	if(ret)
+	{
+		dev_err(&st->spi->dev, "Failed at ADC NOP 1");
+		return ret;
+	}
+
+	ret = ad5592r_s_nop(st, &rx);
+	if(ret)
+	{
+		dev_err(&st->spi->dev, "Failed at ADC NOP 2");
+		return ret;
+	}
+
+	tmp = get_unaligned_be16(&rx);
+
+	addr = tmp;
+	addr &= AD5592R_S_ADC_ADDR_MASK;
+	addr = (addr >>12);
+	dev_info(&st->spi->dev,"req chan = 0x%x; recv chan = 0x%x", chan, addr);
+
+	if(addr != chan){
+		dev_err(&st->spi->dev, "Requested channel doesn't match read channel");
+		return -EINVAL;
+	}
+
+	tmp &= AD5592R_S_ADC_VAL_MASK;
+	*val = tmp;
+	return 0;
+}
+
 static int ad5592r_s_read_raw(struct iio_dev *indio_dev,
 			      struct iio_chan_spec const *chan,
 			      int *val,
 			      int *val2,
 			      long mask)
 {
+	struct ad5592r_s_state *st = iio_priv(indio_dev);
+	int ret;
+
 	switch (mask) {
-	case IIO_CHAN_INFO_ENABLE:
-		*val = 0;
-		return IIO_VAL_INT;
 	case IIO_CHAN_INFO_RAW:
-		if (chan->channel)
-			*val = 0;
-		else
-			*val = 0;
+		ret = ad5592r_s_spi_read_adc(st, chan->channel, val);
+		if(ret)
+		{
+			dev_err(&st->spi->dev, "Error at read adc %d", 
+				chan->channel);
+			return ret;
+		}
 		return IIO_VAL_INT;
 	}
 	return -EINVAL;
@@ -181,10 +255,43 @@ static const struct iio_chan_spec ad5592r_s_channels[] = {
 	}
 };
 
+static int ad5592r_s_init(struct iio_dev *indio_dev)
+{
+	struct ad5592r_s_state *st = iio_priv(indio_dev);
+	int ret;
+
+	ret = ad5592r_s_spi_write_ctl(st, AD5592R_S_REG_SW_RST, 
+				      AD5592R_S_MASK_RST);
+	if(ret)
+	{
+		dev_err(&st->spi->dev, "Failed at reset");
+		return ret;
+	}
+	usleep_range(250, 300);
+
+	ret = ad5592r_s_spi_write_ctl(st, AD5592R_S_REG_REF_CFG,
+				    AD5592R_S_MASK_REF_EN);
+	if(ret)
+	{
+		dev_err(&st->spi->dev, "Failed at power reg write");
+		return ret;
+	}
+
+	ret = ad5592r_s_spi_write_ctl(st, AD5592R_S_REG_ADC_CFG, 
+				      AD5592R_S_ADC_DEF_PIN_MASK);
+	if(ret)
+	{
+		dev_err(&st->spi->dev, "ADC pin reg write failed");
+		return ret;
+	}
+	return 0;
+}
+
 static int ad5592r_s_probe(struct spi_device *spi)
 {
 	struct iio_dev *indio_dev;
 	struct ad5592r_s_state *st;
+	int ret;
 
 	indio_dev = devm_iio_device_alloc(&spi->dev, sizeof(*st));
 
@@ -200,6 +307,13 @@ static int ad5592r_s_probe(struct spi_device *spi)
 	indio_dev->info = &ad5592r_s_info;
 
 	st->spi = spi;
+
+	ret = ad5592r_s_init(indio_dev);
+	if(ret)
+	{
+		dev_err(&st->spi->dev, "Failed at init");
+		return ret;
+	}
 
 	return devm_iio_device_register(&spi->dev, indio_dev);
 }
