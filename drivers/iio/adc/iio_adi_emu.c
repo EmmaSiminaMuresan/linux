73559c12a061ee01de4e661bc67ff4530f356c8b
Popa Cristian Mihai <cristianmihai.popa@analog.com>
2023-08-17 11:43:08 +0300

    iio : adc : iio_adi_emu: Modified read and write raw functions
    -Modified: Eliminated the tmp channels, replaced them with spi_read/write.
    -Created a new function that encapsulates the code used for reading from the ADC.
    In here, we read from both channel 0 and 1 and then we return the result after
    we shifted the data from channel 0(high) and concatened it with data from channel 0
    (low)

--- drivers/iio/adc/iio_adi_emu.c
+++ drivers/iio/adc/iio_adi_emu.c
@@ -15,10 +15,22 @@
 #define ADI_EMU_ADDR_MASK	GENMASK(14,8)
 #define ADI_EMU_VAL_MASK	GENMASK(7, 0)
 
+#define ADI_EMU_REG_CNVST	0x3
+#define ADI_EMU_MASK_CNVST	BIT(0)
+
+#define ADI_EMU_REG_CH0_DATA_LOW	0x4
+#define ADI_EMU_REG_CH0_DATA_HIGH	0x5
+
+#define ADI_EMU_REG_CH1_DATA_LOW	0x6
+#define ADI_EMU_REG_CH1_DATA_HIGH	0x7
+
+#define ADI_EMU_REG_DEVICE_CONFIG	0x2
+#define ADI_EMU_MASK_PWD		BIT(5)
+
 struct adi_emu_state{
 	bool en;
-	u16 tmp_chan0;
-	u16 tmp_chan1;
+	
+
 
 	struct spi_device *spi;
 };
@@ -84,6 +96,66 @@ static int adi_emu_spi_write(struct adi_emu_state *st, u8 reg, u8 val)
 	return spi_sync_transfer(st->spi, &xfer, 1);
 }
 
+static int adi_emu_read_adc(struct adi_emu_state *st, int chan, int *val)
+{
+	int ret;
+	u8 high;
+	u8 low;
+
+	ret = adi_emu_spi_write(st, 
+				ADI_EMU_REG_CNVST, 
+				ADI_EMU_MASK_CNVST);
+
+	if(ret){
+		dev_err(&st->spi->dev, 
+		"Error at conversion");
+		return ret;
+	}
+
+	if(chan)
+	{
+		ret = adi_emu_spi_read(st, 
+				ADI_EMU_REG_CH1_DATA_HIGH,
+				&high);
+		if(ret){
+			dev_err(&st->spi->dev, 
+			"Error at ch1 high read");
+			return ret;
+		}
+
+		ret = adi_emu_spi_read(st, 
+				ADI_EMU_REG_CH1_DATA_LOW,
+				&low);
+		if(ret){
+			dev_err(&st->spi->dev, 
+			"Error at ch1 low read");
+			return ret;
+		}
+	} else {
+		ret = adi_emu_spi_read(st, 
+				ADI_EMU_REG_CH0_DATA_HIGH,
+				&high);
+		if(ret){
+			dev_err(&st->spi->dev, 
+			"Error at ch0 high read");
+			return ret;
+		}
+
+		ret = adi_emu_spi_read(st, 
+				ADI_EMU_REG_CH0_DATA_LOW,
+				&low);
+		if(ret){
+			dev_err(&st->spi->dev, 
+			"Error at ch0 low read");
+			return ret;
+		}
+	}
+	
+	*val = (high << 8) | low;
+
+	return 0;
+}
+
 static int adi_emu_read_raw(struct iio_dev *indio_dev,
 			struct iio_chan_spec const *chan,
 			int *val,
@@ -91,13 +163,19 @@ static int adi_emu_read_raw(struct iio_dev *indio_dev,
 			long mask)
 {
 	struct adi_emu_state *st = iio_priv(indio_dev);
+	
+	int ret;
+
 	switch(mask){
 		case IIO_CHAN_INFO_RAW:
-			if(chan->channel)
-				*val = st->tmp_chan1;
-			else
-				*val = st->tmp_chan0;
-			
+
+			ret = adi_emu_read_adc(st, chan->channel, val);
+			if(ret){
+				dev_err(&st->spi->dev, 
+				"Error at read adc %d", chan->channel);
+				return ret;
+			}
+						
 			return IIO_VAL_INT;
 		case IIO_CHAN_INFO_ENABLE:
 			*val = st->en;
@@ -114,17 +192,21 @@ static int adi_emu_write_raw(struct iio_dev *indio_dev,
 			     int val2,
 			     long mask)
 {
-	struct adi_emu_state *st = iio_priv(indio_dev);	
+	struct adi_emu_state *st = iio_priv(indio_dev);
+	int ret;
+		
 	switch (mask)
 	{
-		case IIO_CHAN_INFO_RAW:
-			if(chan->channel)
-				st->tmp_chan1 = val;
-			else
-				st->tmp_chan0 = val;
-			return 0;
 		case IIO_CHAN_INFO_ENABLE:
 			st->en = val;
+			ret =  adi_emu_spi_write(st, 
+					ADI_EMU_REG_DEVICE_CONFIG, 
+					FIELD_PREP(ADI_EMU_MASK_PWD, !val));
+			if(ret){
+				dev_err(&st->spi->dev, 
+				"Error at writting enable");
+				return ret;
+			}
 			return 0;
 		default:
 			return -EINVAL;
@@ -155,7 +237,8 @@ static const struct iio_chan_spec adi_emu_channel[] ={
 		.channel = 0,
 		.indexed = 1,
 		.info_mask_separate = BIT(IIO_CHAN_INFO_RAW),
-		.info_mask_shared_by_all = BIT(IIO_CHAN_INFO_ENABLE), //suficient sa scriem pt un singur canal
+		.info_mask_shared_by_all = BIT(IIO_CHAN_INFO_ENABLE), 
+		//suficient sa scriem pt un singur canal
 	},
 	{
 		.type = IIO_VOLTAGE,
@@ -179,8 +262,6 @@ static int adi_emu_probe(struct spi_device *spi)
 	st = iio_priv(indio_dev);
 	st->spi = spi;
 	st->en = 0;
-	st->tmp_chan0 = 0;
-	st->tmp_chan1 = 0; 
 
 	indio_dev->name = "iio-adi-emu";
 	indio_dev->info = &adi_emu_info;
