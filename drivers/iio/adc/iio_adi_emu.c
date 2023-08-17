cccdf1718b2eacecd09eb3cb12a9717a78680aba
Radu Sabau <radu.sabau@analog.com>
2023-08-17 11:49:06 +0300

    iio : adc : iio_adi_emu: Modified read and write raw functions
    -Modified: Eliminated the tmp channels, replaced them with spi_read/write.
    -Created a new function that encapsulates the code used for reading from the ADC.
    In here, we read from both channel 0 and 1 and then we return the result after
    we shifted the data from channel 0(high) and concatened it with data from channel 0
    (low)

--- drivers/iio/adc/iio_adi_emu.c
+++ drivers/iio/adc/iio_adi_emu.c
@@ -5,52 +5,66 @@
  * Copyright (C) 2023 Analog Devices, Inc.
  */
 
-#include <asm/unaligned.h>
-#include <linux/bitfield.h>
 #include <linux/module.h>
 #include <linux/spi/spi.h>
-
 #include <linux/iio/iio.h>
+#include <linux/bitfield.h>
+#include <asm/unaligned.h>
 
 #define ADI_EMU_RD_MASK 	BIT(7)
 #define ADI_EMU_ADDR_MASK	GENMASK(14,8)
-#define ADI_EMU_VAL_MASK	GENMASK(7,0)
+#define ADI_EMU_VAL_MASK	GENMASK(7, 0)
 
-struct adi_emu_state {
-	struct spi_device *spi;
+#define ADI_EMU_REG_CNVST	0x3
+#define ADI_EMU_MASK_CNVST	BIT(0)
+
+#define ADI_EMU_REG_CH0_DATA_LOW	0x4
+#define ADI_EMU_REG_CH0_DATA_HIGH	0x5
+
+#define ADI_EMU_REG_CH1_DATA_LOW	0x6
+#define ADI_EMU_REG_CH1_DATA_HIGH	0x7
+
+#define ADI_EMU_REG_DEVICE_CONFIG	0x2
+#define ADI_EMU_MASK_PWD		BIT(5)
+
+struct adi_emu_state{
 	bool en;
-	u16 tmp_chan0;
-	u16 tmp_chan1;
+	
+
+
+	struct spi_device *spi;
 };
 
 static int adi_emu_spi_read(struct adi_emu_state *st, u8 reg, u8 *val)
 {
 	u8 tx = 0;
 	u8 rx = 0;
-	int ret;
+	int ret; 
+
 	struct spi_transfer xfer[] = {
 		{
 			.tx_buf = NULL,
 			.rx_buf = NULL,
 			.len = 1,
-			.cs_change = 1
+			.cs_change = 1,
 		},
 		{
 			.tx_buf = NULL,
 			.rx_buf = NULL,
 			.len = 1,
-			// 
 		}
 	};
 
-	// tx = reg
+	tx = reg;
 	tx |= ADI_EMU_RD_MASK;
-	tx |= reg;
+
+	dev_info(&st->spi->dev, "tx at read = 0%x", tx);
 
 	xfer[0].tx_buf = &tx;
 	xfer[1].rx_buf = &rx;
 
 	ret = spi_sync_transfer(st->spi, xfer, 2);
+
 	if(ret)
 		return ret;
 	
@@ -63,43 +77,114 @@ static int adi_emu_spi_write(struct adi_emu_state *st, u8 reg, u8 val)
 {
 	u16 msg = 0;
 	u16 tx = 0;
-	struct spi_transfer xfer = {
+	struct spi_transfer xfer ={
 		.tx_buf = NULL,
 		.len = 2,
 	};
 
 	msg |= FIELD_PREP(ADI_EMU_ADDR_MASK, reg);
-	msg |= FIELD_PREP(ADI_EMU_VAL_MASK,val);
-	// tx = reg << 8;
+	msg |= FIELD_PREP(ADI_EMU_VAL_MASK, val);
+
+	dev_info(&st->spi->dev, "msg = 0%x", msg);
+
 	put_unaligned_be16(msg, &tx);
+	
+	dev_info(&st->spi->dev, "tx at write = 0%x", tx);
 
 	xfer.tx_buf = &tx;
-
+	
 	return spi_sync_transfer(st->spi, &xfer, 1);
 }
 
+static int adi_emu_read_adc(struct adi_emu_state *st, int chan, int *val)
+{
+	int ret;
+	u8 high;
+	u8 low;
+
+	ret = adi_emu_spi_write(st, 
+				ADI_EMU_REG_CNVST, 
+				ADI_EMU_MASK_CNVST);
+
+	if(ret){
+		dev_err(&st->spi->dev, 
+		"Error at conversion");
+		return ret;
+	}
+
+	if(chan)
+	{
+		ret = adi_emu_spi_read(st, 
+				ADI_EMU_REG_CH1_DATA_HIGH,
+				&high);
+		if(ret){
+			dev_err(&st->spi->dev, 
+			"Error at ch1 high read");
+			return ret;
+		}
+
+		ret = adi_emu_spi_read(st, 
+				ADI_EMU_REG_CH1_DATA_LOW,
+				&low);
+		if(ret){
+			dev_err(&st->spi->dev, 
+			"Error at ch1 low read");
+			return ret;
+		}
+	} else {
+		ret = adi_emu_spi_read(st, 
+				ADI_EMU_REG_CH0_DATA_HIGH,
+				&high);
+		if(ret){
+			dev_err(&st->spi->dev, 
+			"Error at ch0 high read");
+			return ret;
+		}
+
+		ret = adi_emu_spi_read(st, 
+				ADI_EMU_REG_CH0_DATA_LOW,
+				&low);
+		if(ret){
+			dev_err(&st->spi->dev, 
+			"Error at ch0 low read");
+			return ret;
+		}
+	}
+	
+	*val = (high << 8) | low;
+
+	return 0;
+}
+
 static int adi_emu_read_raw(struct iio_dev *indio_dev,
-    struct iio_chan_spec const *chan,
-			    int *val,
-			    int *val2,
-			    long mask)
+			struct iio_chan_spec const *chan,
+			int *val,
+			int *val2,
+			long mask)
 {
 	struct adi_emu_state *st = iio_priv(indio_dev);
-	switch(mask) {
-	case IIO_CHAN_INFO_RAW:
-		if(chan->channel)
-			*val = st->tmp_chan1;
-		else
-			*val = st->tmp_chan0;
-		return IIO_VAL_INT;
-	case IIO_CHAN_INFO_ENABLE:
-		*val = st->en;
-		return IIO_VAL_INT;
 	
-	default :
-		return -EINVAL;
+	int ret;
+
+	switch(mask){
+		case IIO_CHAN_INFO_RAW:
+
+			ret = adi_emu_read_adc(st, chan->channel, val);
+			if(ret){
+				dev_err(&st->spi->dev, 
+				"Error at read adc %d", chan->channel);
+				return ret;
+			}
+						
+			return IIO_VAL_INT;
+		case IIO_CHAN_INFO_ENABLE:
+			*val = st->en;
+			return IIO_VAL_INT;
+		default:
+			return -EINVAL;
 	}
-}
+};
+
 
 static int adi_emu_write_raw(struct iio_dev *indio_dev,
 			     struct iio_chan_spec const *chan,
@@ -108,37 +193,36 @@ static int adi_emu_write_raw(struct iio_dev *indio_dev,
 			     long mask)
 {
 	struct adi_emu_state *st = iio_priv(indio_dev);
-
-
-	switch(mask)
+	int ret;
+		
+	switch (mask)
 	{
-		case IIO_CHAN_INFO_RAW:
-			if(chan->channel)
-				st->tmp_chan1 = val;
-			else
-				st->tmp_chan0 = val;
-			return 0;
 		case IIO_CHAN_INFO_ENABLE:
 			st->en = val;
+			ret =  adi_emu_spi_write(st, 
+					ADI_EMU_REG_DEVICE_CONFIG, 
+					FIELD_PREP(ADI_EMU_MASK_PWD, !val));
+			if(ret){
+				dev_err(&st->spi->dev, 
+				"Error at writting enable");
+				return ret;
+			}
 			return 0;
 		default:
 			return -EINVAL;
 	}
-}
+};
 
 static int adi_emu_reg_acces(struct iio_dev *indio_dev,
-			     unsigned reg, unsigned writeval,
-			     unsigned *readval)
+				unsigned reg, unsigned writeval,
+				unsigned *readval)
 {
 	struct adi_emu_state *st = iio_priv(indio_dev);
 
 	if(readval)
-	{
+		return adi_emu_spi_read(st, reg, (u8 *)readval);
 
-		return adi_emu_spi_read(st,  reg,  (u8 *)readval);
-	}
-
-	return  adi_emu_spi_write(st, reg, writeval);
+	return adi_emu_spi_write(st, reg, writeval);	
 }
 
 static const struct iio_info adi_emu_info = {
@@ -147,14 +231,14 @@ static const struct iio_info adi_emu_info = {
 	.debugfs_reg_access = &adi_emu_reg_acces,
 };
 
-
-static const struct iio_chan_spec adi_emu_channel[] = {
+static const struct iio_chan_spec adi_emu_channel[] ={
 	{
 		.type = IIO_VOLTAGE,
 		.channel = 0,
 		.indexed = 1,
 		.info_mask_separate = BIT(IIO_CHAN_INFO_RAW),
-		.info_mask_shared_by_all = BIT(IIO_CHAN_INFO_ENABLE),
+		.info_mask_shared_by_all = BIT(IIO_CHAN_INFO_ENABLE), 
+		//suficient sa scriem pt un singur canal
 	},
 	{
 		.type = IIO_VOLTAGE,
@@ -163,6 +247,8 @@ static const struct iio_chan_spec adi_emu_channel[] = {
 		.info_mask_separate = BIT(IIO_CHAN_INFO_RAW),
 	}
 };
+
+
 
 static int adi_emu_probe(struct spi_device *spi)
 {
@@ -173,12 +259,9 @@ static int adi_emu_probe(struct spi_device *spi)
 	if(!indio_dev)
 		return -ENOMEM;
 	
-	
 	st = iio_priv(indio_dev);
 	st->spi = spi;
 	st->en = 0;
-	st->tmp_chan0 = 0;
-	st->tmp_chan1 = 0;
 
 	indio_dev->name = "iio-adi-emu";
 	indio_dev->info = &adi_emu_info;
@@ -192,10 +275,11 @@ static struct spi_driver adi_emu_driver = {
 	.driver = {
 		.name = "iio-adi-emu",
 	},
-	.probe = adi_emu_probe
+	.probe = adi_emu_probe,
 };
 
-module_spi_driver(adi_emu_driver);
+module_spi_driver (adi_emu_driver);
+
 
 MODULE_AUTHOR("Radu bogdan Sabau <Radu.Sabau@analog.com");
 MODULE_DESCRIPTION("Analog Devices ADC Emulator Driver");
